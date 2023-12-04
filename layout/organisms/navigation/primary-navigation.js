60b73c2f7f04383769cbfc7bcf8d801985786ba6
tech_e <chris.durfee@dentalqore.com>
2023-12-03 21:03:15 -0700

    updated primary navigation.

--- layout/organisms/navigation/primary-navigation.js
+++ layout/organisms/navigation/primary-navigation.js
@@ -202,10 +202,6 @@ export class PrimarySubNavigation extends SubNavigation
 		for (let i = 0, length = subs.length; i < length; i++)
 		{
 			const sub = subs[i];
-			if (!sub.panel)
-			{
-				continue;
-			}
 			this.appNav.appendChild(sub.panel);
 		}
 	}
