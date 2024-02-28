831b382b46be8617c791026b3494eecda12e6346
tech_e <chris.durfee@dentalqore.com>
2024-02-27 23:44:16 -0700

    Refactor module import and export

--- layout/app-controller.js
+++ layout/app-controller.js
@@ -1,50 +1,8 @@
 import { base, Builder } from "@base-framework/base";
-import "../layout/modules/modules.js";
+import { AddModules } from "../layout/modules/modules.js";
 import { Configs } from "./configs.js";
 import { AppModules } from "./modules/module.js";
 import { AppShell } from "./shell/app-shell.js";
-
-/**
- * This will add the modules to the app.
- *
- * @param {array} modules
- * @return {object}
- */
-const AddModules = (modules) =>
-{
-	if (!modules || modules.length < 1)
-	{
-		return {};
-	}
-
-	let appRoutes = [];
-	let appLinks = [];
-
-	modules.forEach((module) =>
-	{
-		if (!module)
-		{
-			return;
-		}
-
-		const routes = module.getRoutes();
-		if (routes)
-		{
-			appRoutes = appRoutes.concat(routes);
-		}
-
-		const links = module.getLinks();
-		if (links)
-		{
-			appLinks = appLinks.concat(links);
-		}
-	});
-
-	return {
-		routes: appRoutes,
-		links: appLinks
-	};
-};
 
 /**
  * AppController
--- layout/modules/modules.js
+++ layout/modules/modules.js
@@ -1,1 +1,43 @@
 import "./main/module.js";
+
+/**
+ * This will add the modules to the app.
+ *
+ * @param {array} modules
+ * @return {object}
+ */
+export const AddModules = (modules) =>
+{
+	if (!modules || modules.length < 1)
+	{
+		return {};
+	}
+
+	let appRoutes = [];
+	let appLinks = [];
+
+	modules.forEach((module) =>
+	{
+		if (!module)
+		{
+			return;
+		}
+
+		const routes = module.getRoutes();
+		if (routes)
+		{
+			appRoutes = appRoutes.concat(routes);
+		}
+
+		const links = module.getLinks();
+		if (links)
+		{
+			appLinks = appLinks.concat(links);
+		}
+	});
+
+	return {
+		routes: appRoutes,
+		links: appLinks
+	};
+};
