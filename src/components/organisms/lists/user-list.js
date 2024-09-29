1df44fae3130667b5507466e4aec6b1650d1f178
tech_e <chris.durfee@dentalqore.com>
2024-09-29 17:57:48 -0600

    refactor: Improve formatting and structure of user-list component

--- src/components/organisms/lists/user-list.js
+++ src/components/organisms/lists/user-list.js
@@ -3,54 +3,58 @@ import { Atom } from '@base-framework/base';
 
 // Sample User Data (this could come from a backend or be passed as props)
 const users = [
-  {
-    name: 'Leslie Alexander',
-    email: 'leslie.alexander@example.com',
-    image: 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    role: 'Co-Founder / CEO',
-    lastSeen: '2023-01-23T13:23Z',
-    status: 'offline'
-  },
-  {
-    name: 'Michael Foster',
-    email: 'michael.foster@example.com',
-    image: 'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    role: 'Co-Founder / CTO',
-    lastSeen: '2023-01-23T13:23Z',
-    status: 'offline'
-  },
-  {
-    name: 'Dries Vincent',
-    email: 'dries.vincent@example.com',
-    image: 'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    role: 'Business Relations',
-    status: 'online'
-  },
-  // Add more users as necessary
+    {
+        name: 'Leslie Alexander',
+        email: 'leslie.alexander@example.com',
+        image: 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        role: 'Co-Founder / CEO',
+        lastSeen: '2023-01-23T13:23Z',
+        status: 'offline'
+    },
+    {
+        name: 'Michael Foster',
+        email: 'michael.foster@example.com',
+        image: 'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        role: 'Co-Founder / CTO',
+        lastSeen: '2023-01-23T13:23Z',
+        status: 'offline'
+    },
+    {
+        name: 'Dries Vincent',
+        email: 'dries.vincent@example.com',
+        image: 'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        role: 'Business Relations',
+        status: 'online'
+    }
 ];
 
 /**
  * User Image Atom
+ *
+ * @param {object} props
+ * @returns {object}
  */
-const UserImage = Atom(({ src, alt }) => {
-  return Img({
-    class: 'h-12 w-12 flex-none rounded-full bg-gray-50',
-    src: src,
-    alt: alt
-  });
+const UserImage = Atom(({ src, alt }) =>
+{
+    return Img({
+        class: 'h-12 w-12 flex-none rounded-full bg-gray-50',
+        src,
+        alt
+    });
 });
 
 /**
  * User Info Atom
+ *
+ * @param {object} props
+ * @returns {object}
  */
-const UserInfo = Atom(({ name, email }) => {
-  return Div(
-    { class: 'min-w-0 flex-auto' },
-    [
-      P({ class: 'text-sm font-semibold leading-6 text-gray-900' }, name),
-      P({ class: 'mt-1 truncate text-xs leading-5 text-gray-500' }, email)
-    ]
-  );
+const UserInfo = Atom(({ name, email }) =>
+{
+    return Div({ class: 'min-w-0 flex-auto' }, [
+        P({ class: 'text-sm font-semibold leading-6 text-gray-900' }, name),
+        P({ class: 'mt-1 truncate text-xs leading-5 text-gray-500' }, email)
+    ]);
 });
 
 /**
@@ -58,7 +62,7 @@ const UserInfo = Atom(({ name, email }) => {
  *
  * @returns {object}
  */
-const UserOnline = Atom(() =>
+const UserOnline = () =>
 {
     return Div({ class: 'mt-1 flex items-center gap-x-1.5' }, [
             Div({ class: 'flex-none rounded-full bg-emerald-500/20 p-1' }, [
@@ -67,7 +71,7 @@ const UserOnline = Atom(() =>
             P({ class: 'text-xs leading-5 text-gray-500' }, 'Online')
         ]
     );
-});
+};
 
 /**
  * User Offline Atom
@@ -84,16 +88,28 @@ const UserOffline = (lastSeen) =>
 };
 
 /**
+ * This will get the user status.
+ *
+ * @param {string} status
+ * @param {string} lastSeen
+ * @returns {object}
+ */
+const getStatus = (status, lastSeen) =>
+{
+    return status === 'online'
+        ? UserOnline()
+        : UserOffline(lastSeen);
+};
+
+/**
  * User Status Atom
  */
-const UserStatus = Atom(({ role, lastSeen, status }) => {
-  return Div({ class: 'hidden shrink-0 sm:flex sm:flex-col sm:items-end' }, [
-      P({ class: 'text-sm leading-6 text-gray-900' }, role),
-      status === 'online'
-        ? UserOnline()
-        : UserOffline({ lastSeen })
-    ]
-  );
+const UserStatus = Atom(({ role, lastSeen, status }) =>
+{
+    return Div({ class: 'hidden shrink-0 sm:flex sm:flex-col sm:items-end' }, [
+        P({ class: 'text-sm leading-6 text-gray-900' }, role),
+        getStatus(status, lastSeen)
+    ]);
 });
 
 /**
@@ -102,7 +118,8 @@ const UserStatus = Atom(({ role, lastSeen, status }) => {
  * @param {object} props
  * @returns {object}
  */
-const UserListItem = Atom(({ user }) => {
+const UserListItem = Atom(({ user }) =>
+{
     return Li({ class: 'flex justify-between gap-x-6 py-5' }, [
         Div({ class: 'flex min-w-0 gap-x-4' }, [
             UserImage({ src: user.image, alt: user.name }),
@@ -122,15 +139,16 @@ const UserListItem = Atom(({ user }) => {
  * @param {object} props
  * @returns {object}
  */
-const UserList = Atom((props) => {
-  return Ul(
-    {
-        ...props,
-        role: 'list',
-        class: 'divide-y divide-gray-100',
-        map: [props.items, UserListItem]
-    }
-  );
+const UserList = Atom((props) =>
+{
+    return Ul(
+        {
+            ...props,
+            role: 'list',
+            class: 'divide-y divide-gray-100',
+            map: [props.items, UserListItem]
+        }
+    );
 });
 
 export default UserList;
