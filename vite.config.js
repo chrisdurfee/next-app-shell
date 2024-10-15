656af5e53d13132baa34e4b08eda3b315e91fd1e
tech_e <chris.durfee@dentalqore.com>
2024-10-14 23:42:39 -0600

    feat: Integrate Tailwind CSS and configure theming with custom properties

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
+import autoprefixer from 'autoprefixer';
+import postcss from 'postcss';
+import tailwindcss from 'tailwindcss';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [],
+	plugins: [
+		postcss({
+			plugins: [tailwindcss(), autoprefixer()],
+		})
+	],
 	base: '/next-app-shell/',
 	server: {
 		open: true
