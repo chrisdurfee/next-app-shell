c82b259615dc0cb30326c4650e6e559c665e966c
tech_e <chris.durfee@dentalqore.com>
2024-10-14 23:50:09 -0600

    refactor: Remove Tailwind CSS setup from base.css and update Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,8 @@
-import autoprefixer from 'autoprefixer';
-import postcss from 'postcss';
-import tailwindcss from 'tailwindcss';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [
-		postcss({
-			plugins: [tailwindcss(), autoprefixer()],
-		})
-	],
+	plugins: [],
 	base: '/next-app-shell/',
 	server: {
 		open: true
