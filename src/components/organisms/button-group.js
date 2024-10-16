543442a4ffa20390bae4f0c8d29bcb6a32816f55
tech_e <chris.durfee@dentalqore.com>
2024-10-16 16:04:29 -0600

    style: Update button-group component; adjust button padding and navigation margin

--- src/components/organisms/button-group.js
+++ src/components/organisms/button-group.js
@@ -22,7 +22,7 @@ const Buttons = Atom((props, children) => ({
  */
 const StateButton = Atom(({ value, label }) => (
 	Button({
-		class: 'm-1 inline-flex items-center justify-center whitespace-nowrap rounded-md px-3 py-1 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-background data-[state=active]:text-foreground data-[state=active]:shadow text-zinc-600 dark:text-zinc-200',
+		class: 'inline-flex items-center justify-center whitespace-nowrap rounded-md px-8 py-1 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-background data-[state=active]:text-foreground data-[state=active]:shadow',
 		onState: ['performance', { active: value}],
 		dataSet: ['performance', ['state', value, 'active']],
 		click: (e, {state}) => state.performance = value
