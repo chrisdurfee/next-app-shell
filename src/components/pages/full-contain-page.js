fdaad96022aaa0c41079e715abf2928a2c7089a9
tech_e <chris.durfee@dentalqore.com>
2024-10-22 18:06:45 -0600

    style: Update class names in FullContainPage for improved layout and responsiveness

--- src/components/pages/full-contain-page.js
+++ src/components/pages/full-contain-page.js
@@ -21,7 +21,7 @@ export class FullContainPage extends BasicPage
 	{
 		return MainSection([
 			FullTemplate([
-				Div({ class: 'contained px-4 sm:px-0' }, this.children)
+				Div({ class: 'contained px-4 flex flex-auto flex-col' }, this.children)
 			])
 		]);
 	}
