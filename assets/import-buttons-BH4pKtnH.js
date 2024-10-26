a9da8da950de454bc5c4343a208e7fcbd2aebce0
chrisdurfee <chrisdurfee@users.noreply.github.com>
2024-10-26 22:50:34 +0000

    deploy: c4e32aec950865792bcf8dc0a9e6d0df1aff8b8a

--- assets/import-buttons-BH4pKtnH.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/button-group-D4ASIDpt.js","assets/index-C5sUUiBP.js","assets/index-hC_xMdla.css"])))=>i.map(i=>d[i]);
-import{X as t,m as s,e,S as o,F as r,_ as i}from"./index-C5sUUiBP.js";const c=t((l,a)=>s([e({class:"scroll-m-20 text-lg font-bold tracking-tight"},"Label"),o({class:"text-muted-foreground"},"This section will test the import system."),r({src:i(()=>import("./button-group-D4ASIDpt.js"),__vite__mapDeps([0,1,2]))})]));export{c as default};
--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 importScripts('./worker/cache-controller.js', './worker/service.js');
 
 const appName = 'base-example',
-version = '0.0.25';
+version = '0.0.26';
 
 const files =
 [
