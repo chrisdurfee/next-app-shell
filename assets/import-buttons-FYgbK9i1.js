93300b16c2c1495e1cd53f9fbeb142f2b8c8a331
chrisdurfee <chrisdurfee@users.noreply.github.com>
2024-10-28 23:12:59 +0000

    deploy: 2e49510600bbe8d696098d0461d5bf58bdb095cc

---
+++ assets/import-buttons-FYgbK9i1.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/button-group-C8a3oI_o.js","assets/index-R1cPadIn.js","assets/index-C_R6olEu.css"])))=>i.map(i=>d[i]);
+import{X as t,m as s,g as e,S as o,F as r,_ as i}from"./index-R1cPadIn.js";const c=t((l,a)=>s([e({class:"scroll-m-20 text-lg font-bold tracking-tight"},"Label"),o({class:"text-muted-foreground"},"This section will test the import system."),r({src:i(()=>import("./button-group-C8a3oI_o.js"),__vite__mapDeps([0,1,2]))})]));export{c as default};
--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 importScripts('./worker/cache-controller.js', './worker/service.js');
 
 const appName = 'base-example',
-version = '0.0.30';
+version = '0.0.31';
 
 const files =
 [
