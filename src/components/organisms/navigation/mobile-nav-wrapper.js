ff6921d2b701e4eda7d39134d4ea1175cbe48a30
tech_e <chris.durfee@dentalqore.com>
2024-10-27 18:44:47 -0600

    style: Update mobile navigation wrapper background class for improved UI consistency

--- src/components/organisms/navigation/mobile-nav-wrapper.js
+++ src/components/organisms/navigation/mobile-nav-wrapper.js
@@ -87,7 +87,7 @@ const ExapandedNavigation = (props) =>
 
 	return Div(
 		{
-			class: 'bg-background flex flex-auto flex-col absolute w-full transition-all h-0 data-[expanded=true]:h-fit data-[expanded=false]:h-0 overflow-hidden data-[expanded=true]:shadow data-[expanded=true]:border-b rounded-md z-20',
+			class: 'bg-popover flex flex-auto flex-col absolute w-full h-0 data-[expanded=true]:h-fit data-[expanded=false]:h-0 overflow-hidden data-[expanded=true]:shadow data-[expanded=true]:border rounded-md z-20',
 			addState()
 			{
 				return {
