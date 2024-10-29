bd1ef5acdbe7b3dd053601b73d097bfbb933a980
tech_e <chris.durfee@dentalqore.com>
2024-10-28 18:54:35 -0600

    feat: Refactor LoginPage to modularize components and enhance layout with sign-up and Google login options

--- src/modules/login/components/pages/login/login-page.js
+++ src/modules/login/components/pages/login/login-page.js
@@ -1,4 +1,4 @@
-import { Div, Form, H1, Header, P, Span } from '@base-framework/atoms';
+import { Div, Form, H1, Header, P, Section, Span } from '@base-framework/atoms';
 import { Atom } from '@base-framework/base';
 import { Button } from '@components/atoms/buttons/buttons.js';
 import { Input } from '@components/atoms/form/input.js';
@@ -17,32 +17,81 @@ const LoginHeader = Atom(({ title, description}) => (
 	])
 ));
 
+/**
+ * This will create a sign up link.
+ * @returns {object}
+ */
+const SignUpLink = () => (
+	Div({ class: '' }, [
+		Span({ class: 'text-sm text-muted-foreground mt-8 mb-0' }, 'Don\'t have an account? '),
+		Span({ class: 'text-sm font-medium text-primary underline' }, 'Sign up'),
+	])
+);
+
+/**
+ * This will create the credentials container.
+ *
+ * @returns {object}
+ */
+const CredentialsContainer = () => (
+	Div({ class: 'grid gap-4' }, [
+		Div({ class: 'grid gap-4' }, [
+			Input( { type: 'text', placeholder: 'Username' } ),
+		]),
+		Div({ class: 'grid gap-4' }, [
+			Input( { type: 'password', placeholder: 'Password' } ),
+		])
+	])
+);
+
+/**
+ * This will create the sign in button.
+ *
+ * @returns {object}
+ */
+const SignInButton = () => (
+	Div({ class: 'grid gap-4' }, [
+		Button({ type: 'submit' }, 'Login')
+	])
+);
+
+/**
+ * This will create the sign in with google button.
+ *
+ * @returns {object}
+ */
+const SignInWIthGoogleButton = () => (
+	Div({ class: 'grid gap-4' }, [
+		Button({ variant: 'outline' }, 'Login with Google')
+	])
+);
+
+/**
+ * This will create the login form.
+ *
+ * @returns {object}
+ */
 const LoginForm = () => (
-	Div({ class: 'flex flex-auto flex-col justify-center items-center' }, [
-		Div({ class: 'rounded-xl border bg-card text-card-foreground shadow mx-auto max-w-sm' }, [
+	Form({ class: 'flex flex-col p-6 pt-0' }, [
+		Div({ class: 'grid gap-4' }, [
+			CredentialsContainer(),
+			SignInButton(),
+			SignInWIthGoogleButton(),
+			SignUpLink(),
+		]),
+	])
+);
+
+/**
+ * This will create the login section.
+ *
+ * @returns {object}
+ */
+const LoginSection = () => (
+	Section({ class: 'flex flex-auto flex-col justify-center items-center' }, [
+		Div({ class: 'rounded-xl border bg-card text-card-foreground shadow w-full mx-auto max-w-sm' }, [
 			LoginHeader({ title: 'Login', description: 'Please enter your credentials to login.' }),
-			Form({ class: 'flex flex-col p-6 pt-0' }, [
-				Div({ class: 'grid gap-4' }, [
-					Div({ class: 'grid gap-4' }, [
-						Input( { type: 'text', placeholder: 'Username' } ),
-					]),
-					Div({ class: 'grid gap-4' }, [
-						Input( { type: 'password', placeholder: 'Password' } ),
-					]),
-					Div({ class: 'grid gap-4' }, [
-						Button({ type: 'submit' }, 'Login')
-					]),
-
-					Div({ class: 'grid gap-4' }, [
-						Button({ variant: 'outline' }, 'Login with Google')
-					]),
-
-					Div({ class: '' }, [
-						Span({ class: 'text-sm text-muted-foreground mt-8 mb-0' }, 'Don\'t have an account? '),
-						Span({ class: 'text-sm font-medium text-primary underline' }, 'Sign up'),
-					]),
-				]),
-			])
+			LoginForm(),
 		])
 	])
 );
@@ -57,7 +106,7 @@ const LoginForm = () => (
 export const LoginPage = () => (
 	new FullPage(
 		[
-			LoginForm()
+			LoginSection()
 		]
 	)
 );
