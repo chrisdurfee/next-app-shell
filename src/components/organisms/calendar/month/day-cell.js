df073ba5ad5e39062d8ca1cf77f8308d42b1f11d
tech_e <chris.durfee@dentalqore.com>
2024-11-01 17:01:01 -0600

    fix: Update class names for today's and current date in DayCell component

--- src/components/organisms/calendar/month/day-cell.js
+++ src/components/organisms/calendar/month/day-cell.js
@@ -6,7 +6,7 @@ import { Button } from '@base-framework/atoms';
  * @param {boolean} isToday - Indicates if the day is today.
  * @returns {string}
  */
-const getTodayClass = (isToday) => (isToday ? 'bg-primary text-primary-foreground' : '');
+const getTodayClass = (isToday) => (isToday ? 'bg-accent text-primary' : '');
 
 /**
  * This will get the class for outside month.
@@ -14,7 +14,7 @@ const getTodayClass = (isToday) => (isToday ? 'bg-primary text-primary-foregroun
  * @param {boolean} isOutsideMonth
  * @returns {string}
  */
-const getOutsideMonthClass = (isOutsideMonth) => (isOutsideMonth ? 'text-muted-foreground opacity-50' : 'text-foreground');
+const getOutsideMonthClass = (isOutsideMonth) => (isOutsideMonth ? 'text-muted-foreground opacity-50' : '');
 
 /**
  * Check if the current date is the same as the date passed.
@@ -32,7 +32,7 @@ const isCurrentDate = (currentDate, date) => currentDate === date;
  * @param {string} date - The date to compare.
  * @returns {string}
  */
-const getCurrentDateClass = (currentDate, date) => (isCurrentDate(currentDate, date) ? 'bg-accent text-accent-foreground' : '');
+const getCurrentDateClass = (currentDate, date) => (isCurrentDate(currentDate, date) ? 'bg-primary text-primary-foreground' : '');
 
 /**
  * This will render a day cell in a calendar.
@@ -45,9 +45,8 @@ export const DayCell = ({ day, currentDate, date, isToday, isOutsideMonth, selec
         {
             class: `
         flex items-center justify-center h-9 w-auto p-0 font-normal text-sm rounded-md
-        ${getTodayClass(isToday)}
+        ${(getTodayClass(isToday) || getCurrentDateClass(currentDate, date))}
         ${getOutsideMonthClass(isOutsideMonth)}
-        ${getCurrentDateClass(currentDate, date)}
         hover:bg-accent hover:text-accent-foreground focus:z-10
       `,
             disabled: day === null,
