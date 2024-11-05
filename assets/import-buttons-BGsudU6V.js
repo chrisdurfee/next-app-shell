ca4af11cbe52c3bbd5ea984a2fa7bbb2e3a5016f
chrisdurfee <chrisdurfee@users.noreply.github.com>
2024-11-05 00:07:20 +0000

    deploy: c375394b3da1cc55e29310b92871956822765105

---
+++ assets/import-buttons-BGsudU6V.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/button-group-CQP2EoXt.js","assets/index-Ddhx32Yx.js","assets/index-B0vrui5z.css"])))=>i.map(i=>d[i]);
+import{X as t,m as s,j as e,S as o,F as r,_ as i}from"./index-Ddhx32Yx.js";const c=t((l,a)=>s([e({class:"scroll-m-20 text-lg font-bold tracking-tight"},"Label"),o({class:"text-muted-foreground"},"This section will test the import system."),r({src:i(()=>import("./button-group-CQP2EoXt.js"),__vite__mapDeps([0,1,2]))})]));export{c as default};
