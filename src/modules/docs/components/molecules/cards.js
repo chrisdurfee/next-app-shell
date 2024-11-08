a83996ffac4168c7565a00fdcb130560bf811293
tech_e <chris.durfee@dentalqore.com>
2024-11-08 16:30:00 -0700

    feat: Enhance CodeCard component to notify users when code is copied to clipboard

--- src/modules/docs/components/molecules/cards.js
+++ src/modules/docs/components/molecules/cards.js
@@ -1,5 +1,6 @@
 import { Code, Div, Pre } from "@base-framework/atoms";
 import { Atom } from "@base-framework/base";
+import { Icons } from "@components/icons/icons.js";
 
 /**
  * This will create a code card.
@@ -10,7 +11,15 @@ import { Atom } from "@base-framework/base";
  */
 export const CodeCard = Atom((props, children) => (
     Pre({ class: 'flex p-4 min-h-[350px] max-h-[650px] overflow-x-auto rounded-lg border bg-muted whitespace-break-spaces break-all cursor-pointer' }, [
-        Code({ class: 'font-mono text-sm text-wrap', click: () => navigator.clipboard.writeText(children[0].textContent) }, children)
+        Code({ class: 'font-mono text-sm text-wrap', click: () =>
+        {
+            navigator.clipboard.writeText(children[0].textContent);
+            app.notify({
+                title: "Code copied",
+                description: "The code has been copied to your clipboard.",
+                icon: Icons.clipboard.checked
+            });
+        }}, children)
     ])
 ));
 
