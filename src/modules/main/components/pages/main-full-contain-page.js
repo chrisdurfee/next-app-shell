164e4d5335b62ee0babe24ea4469ad2df4baf17e
tech_e <chris.durfee@dentalqore.com>
2024-11-07 17:14:12 -0700

    feat: Add sample user data to main full contain page for demonstration purposes

--- src/modules/main/components/pages/main-full-contain-page.js
+++ src/modules/main/components/pages/main-full-contain-page.js
@@ -29,6 +29,29 @@ const users = [
         image: 'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
         role: 'Business Relations',
         status: 'online'
+    },
+    {
+        name: 'Leslie Alexander',
+        email: 'leslie.alexander@example.com',
+        image: 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        role: 'Co-Founder / CEO',
+        lastSeen: '2023-01-23T13:23Z',
+        status: 'offline'
+    },
+    {
+        name: 'Michael Foster',
+        email: 'michael.foster@example.com',
+        image: 'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        role: 'Co-Founder / CTO',
+        lastSeen: '2023-01-23T13:23Z',
+        status: 'offline'
+    },
+    {
+        name: 'Dries Vincent',
+        email: 'dries.vincent@example.com',
+        image: 'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        role: 'Business Relations',
+        status: 'online'
     }
 ];
 
