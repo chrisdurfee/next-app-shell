acb83fa5886995f0e5be7d7845585e14c5f23644
tech_e <chris.durfee@dentalqore.com>
2024-11-07 17:28:16 -0700

    feat: Update DialogContainer styling for improved responsiveness and layout

--- src/components/molecules/dialogs/dialog.js
+++ src/components/molecules/dialogs/dialog.js
@@ -31,7 +31,9 @@ const DialogHeader = ({ title }) => (
  * @returns {object}
  */
 export const DialogContainer = Atom((props, children) => (
-    MainDialog({ class: `flex flex-auto flex-col fixed z-30 w-full max-w-lg gap-4 border bg-background text-foreground p-6 shadow-lg duration-200 sm:rounded-lg`, click: props.click, aria: { expanded: ['open']}}, [
+    MainDialog({ class: `fixed z-30 w-[98%] md:w-full max-w-lg gap-4 border bg-background text-foreground p-6 pb-12 md:pb-6 shadow-lg duration-200
+                rounded-lg flex flex-auto flex-col
+                bottom-4 top-auto md:bottom-auto md:top-1/2 md:left-1/2 md:-translate-x-1/2 md:-translate-y-1/2`, click: props.click, aria: { expanded: ['open']}}, [
         Div({ class: 'flex flex-auto flex-col space-y-2' }, [
             DialogHeader(props),
             props.description && P({ class: 'flex flex-auto flex-col text-sm text-muted-foreground' }, props.description),
