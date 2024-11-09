2ea8c4d9dd2c8546536381848203d46486db5acb
chrisdurfee <chrisdurfee@users.noreply.github.com>
2024-11-09 00:51:43 +0000

    deploy: 29e85053551c094d0b9c2c607a27f65a87ca638b

---
+++ assets/import-buttons-BRs-NVj-.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/button-group-CJv1cDiY.js","assets/index-CbN3SU0V.js","assets/index-CDJdmhYR.css"])))=>i.map(i=>d[i]);
+import{K as t,m as s,j as e,S as o,F as r,_ as i}from"./index-CbN3SU0V.js";const c=t((l,a)=>s([e({class:"scroll-m-20 text-lg font-bold tracking-tight"},"Label"),o({class:"text-muted-foreground"},"This section will test the import system."),r({src:i(()=>import("./button-group-CJv1cDiY.js"),__vite__mapDeps([0,1,2]))})]));export{c as default};
