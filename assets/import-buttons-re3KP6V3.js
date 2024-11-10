2bd41390c5a9ead73d59f521404d7142960b66dc
chrisdurfee <chrisdurfee@users.noreply.github.com>
2024-11-10 10:25:37 +0000

    deploy: bb7df1489e48e40c12ac43fc47c042839a945555

---
+++ assets/import-buttons-re3KP6V3.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/button-group-D1bbzw0j.js","assets/index-naUt7-jP.js","assets/index-KPrbpB54.css"])))=>i.map(i=>d[i]);
+import{K as t,m as s,F as e,S as o,g as r,i}from"./index-naUt7-jP.js";const c=t((l,a)=>s([e({class:"scroll-m-20 text-lg font-bold tracking-tight"},"Label"),o({class:"text-muted-foreground"},"This section will test the import system."),r({src:i(()=>import("./button-group-D1bbzw0j.js"),__vite__mapDeps([0,1,2]))})]));export{c as default};
