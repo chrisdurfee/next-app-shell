50b41659866115a1bcbfbae94e9b786d5bfeaaed
chrisdurfee <chrisdurfee@users.noreply.github.com>
2024-11-11 04:06:06 +0000

    deploy: b28c6889bf1ef62dfcc4f2bf516a2783a9031777

---
+++ assets/import-buttons-4yNgXvTx.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/button-group-CWfdvqDu.js","assets/index-ZmRIzFw4.js","assets/index-BXSdXSf5.css"])))=>i.map(i=>d[i]);
+import{K as t,m as s,F as e,S as o,i as r,j as i}from"./index-ZmRIzFw4.js";const c=t((l,a)=>s([e({class:"scroll-m-20 text-lg font-bold tracking-tight"},"Label"),o({class:"text-muted-foreground"},"This section will test the import system."),r({src:i(()=>import("./button-group-CWfdvqDu.js"),__vite__mapDeps([0,1,2]))})]));export{c as default};
