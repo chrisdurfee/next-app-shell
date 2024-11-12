1e8b6b973fe32747783571f5e0a4037f7af423c9
tech_e <chris.durfee@dentalqore.com>
2024-11-11 18:10:55 -0700

    refactor: Remove MusicHeader component and associated imports from MusicPage for cleaner code structure

--- src/modules/main/components/pages/music/music-page/music-page.js
+++ src/modules/main/components/pages/music/music-page/music-page.js
@@ -1,35 +1,7 @@
 import { Div } from "@base-framework/atoms";
-import { Button } from "@components/atoms/buttons/buttons.js";
-import { Tooltip } from "@components/atoms/tooltip.js";
-import { Icons } from "@components/icons/icons.js";
-import TabGroup from "@components/organisms/tabs/tab-group.js";
 import { BlankPage } from "@components/pages/blank-page.js";
 import { MusicSections } from "./music-sections.js";
 import { SidebarMenu } from "./sidebar-menu.js";
-
-/**
- * This will create a music header with tabs and add music button.
- *
- * @returns {object}
- */
-const MusicHeader = () => (
-    Div({ class: 'justify-between flex flex-auto items-center' }, [
-        new TabGroup({
-            options: [
-                { label: 'Music', value: 'music' },
-                { label: 'Podcasts', value: 'podcasts' },
-                { label: 'Live', value: 'live', disabled: true }
-            ],
-            onSelect: (value, { state }) => state.mode = value
-        }),
-        Div({ class: 'hidden lg:inline-flex' }, [
-            Button({ variant: 'withIcon', class: 'text-muted-foreground mb-8 hidden', icon: Icons.circlePlus }, 'Add music'),
-        ]),
-        Div({ class: 'flex lg:hidden mr-4' }, [
-            Tooltip({ content: 'Add music', position: 'left' }, Button({ variant: 'icon', icon: Icons.circlePlus }))
-        ])
-    ])
-);
 
 /**
  * MusicPage
