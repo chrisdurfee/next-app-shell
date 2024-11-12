6dd19bdc78038cc32172caf22fd7aba6948bd089
chrisdurfee <chrisdurfee@users.noreply.github.com>
2024-11-12 00:35:43 +0000

    deploy: 212bada64b3783e82cde56952226bae73fb9be96

---
+++ assets/import-buttons-DTtlERX2.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/button-group-BbGhMLP-.js","assets/index-BJb2xC5L.js","assets/index-C1pokyST.css"])))=>i.map(i=>d[i]);
+import{K as t,m as s,F as e,a as o,i as r,j as i}from"./index-BJb2xC5L.js";const c=t((a,l)=>s([e({class:"scroll-m-20 text-lg font-bold tracking-tight"},"Label"),o({class:"text-muted-foreground"},"This section will test the import system."),r({src:i(()=>import("./button-group-BbGhMLP-.js"),__vite__mapDeps([0,1,2]))})]));export{c as default};
