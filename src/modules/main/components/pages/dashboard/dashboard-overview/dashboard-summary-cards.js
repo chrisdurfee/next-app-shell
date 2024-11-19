055fdd98ad4ba7441a9c28fd18f965d03cdd1811
tech_e <chris.durfee@dentalqore.com>
2024-11-18 17:09:08 -0700

    feat: Adjust padding and margin in DashboardSummaryCards and MusicSection for improved layout

--- src/modules/main/components/pages/dashboard/dashboard-overview/dashboard-summary-cards.js
+++ src/modules/main/components/pages/dashboard/dashboard-overview/dashboard-summary-cards.js
@@ -8,7 +8,7 @@ import { DashboardCard } from "./dashboard-card.js";
  * @returns {object}
  */
 export const DashboardSummaryCards = () => (
-    Div({ class: 'flex flex-auto overflow-x-auto' }, [
+    Div({ class: 'flex flex-auto overflow-x-auto -mx-6 px-6' }, [
         Div({ class: 'inline-flex flex-auto space-x-4 ml-[-24px] pl-6 mr-6 lg:mr-0' }, [
             DashboardCard({
                 title: 'Total Revenue',
