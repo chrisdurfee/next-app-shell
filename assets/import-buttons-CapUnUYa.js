a1b3a426af1200e298b940a9f39ea53ecdb7f46e
chrisdurfee <chrisdurfee@users.noreply.github.com>
2024-11-22 23:15:41 +0000

    deploy: 42125bfd75844adcf0d67d563d75d48dd9cf9c3e

---
+++ assets/import-buttons-CapUnUYa.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/button-group-CoE3CBxt.js","assets/index-Cwbzy0lu.js","assets/index-BaCtJdiA.css"])))=>i.map(i=>d[i]);
+import{Q as t,m as s,F as e,b as o,V as r,i}from"./index-Cwbzy0lu.js";const c=t((l,a)=>s([e({class:"scroll-m-20 text-lg font-bold tracking-tight"},"Label"),o({class:"text-muted-foreground"},"This section will test the import system."),r({src:i(()=>import("./button-group-CoE3CBxt.js"),__vite__mapDeps([0,1,2]))})]));export{c as default};
