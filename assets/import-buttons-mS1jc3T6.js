970a11a63d8c5b761695d993a06f0484ba57aab9
chrisdurfee <chrisdurfee@users.noreply.github.com>
2024-11-24 06:47:13 +0000

    deploy: a35c52e880a47c3bf71b994d23c5558292107737

---
+++ assets/import-buttons-mS1jc3T6.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/button-group-Dc1omZHG.js","assets/index-DFPLRPsh.js","assets/index-BsVjzHBh.css"])))=>i.map(i=>d[i]);
+import{Q as t,m as s,F as e,b as o,V as r,i}from"./index-DFPLRPsh.js";const c=t((l,a)=>s([e({class:"scroll-m-20 text-lg font-bold tracking-tight"},"Label"),o({class:"text-muted-foreground"},"This section will test the import system."),r({src:i(()=>import("./button-group-Dc1omZHG.js"),__vite__mapDeps([0,1,2]))})]));export{c as default};
