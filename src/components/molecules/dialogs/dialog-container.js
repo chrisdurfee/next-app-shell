422431f3b9382e19e82bf6f15240b78815a32d27
tech_e <chris.durfee@dentalqore.com>
2024-11-23 23:43:57 -0700

    feat: Update DialogContainer styling for improved layout and spacing

--- src/components/molecules/dialogs/dialog-container.js
+++ src/components/molecules/dialogs/dialog-container.js
@@ -22,14 +22,20 @@ const DialogHeader = ({ title }) => (
  * @returns {object}
  */
 export const DialogContainer = Atom((props, children) => (
-    MainDialog({ class: `fixed pullUpIn z-30 w-[98%] md:w-full max-w-lg gap-4 border bg-background text-foreground p-6 pb-12 md:pb-6 shadow-lg duration-200
+    MainDialog({
+            class: `fixed pullUpIn z-30 w-[98%] md:w-full max-w-lg border bg-popover text-foreground shadow-lg duration-200
                 rounded-lg flex flex-auto flex-col
-                bottom-4 top-auto md:m-auto md:bottom-auto md:top-1/2 md:left-0 md:right-0 md:-translate-y-1/2`, click: props.click, aria: { expanded: ['open']}}, [
-        Div({ class: 'flex flex-auto flex-col space-y-2' }, [
-            DialogHeader(props),
-            props.description && P({ class: 'flex flex-auto flex-col text-sm text-muted-foreground' }, props.description),
-            Div({ class: 'flex flex-auto flex-col text-sm text-muted-foreground' }, children),
-        ]),
-        Footer({ class: 'flex flex-col-reverse sm:flex-row sm:justify-end mt-6 gap-2 sm:gap-0 sm:space-x-2' }, props.buttons)
+                bottom-4 top-auto md:m-auto md:bottom-auto md:top-1/2 md:left-0 md:right-0 md:-translate-y-1/2`,
+            click: props.click,
+            aria: { expanded: ['open'] }
+        }, [
+        Div({ class: 'flex flex-auto flex-col gap-4 p-6 pb-12 md:pb-6' }, [
+            Div({ class: 'flex flex-auto flex-col space-y-2' }, [
+                DialogHeader(props),
+                props.description && P({ class: 'flex flex-auto flex-col text-sm text-muted-foreground' }, props.description),
+                Div({ class: 'flex flex-auto flex-col text-sm text-muted-foreground' }, children),
+            ]),
+            Footer({ class: 'flex flex-col-reverse sm:flex-row sm:justify-end mt-6 gap-2 sm:gap-0 sm:space-x-2' }, props.buttons)
+        ])
     ])
 ));
