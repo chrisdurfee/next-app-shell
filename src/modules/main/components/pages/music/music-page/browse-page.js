6c606aa05ca84df6a011cfca4f903356d4a1b360
tech_e <chris.durfee@dentalqore.com>
2024-11-24 16:01:45 -0700

    feat: Update service worker version and add BrowsePage component to MusicPage

---
+++ src/modules/main/components/pages/music/music-page/browse-page.js
@@ -0,0 +1,22 @@
+import { Div, H2, P } from "@base-framework/atoms";
+import { Icon } from "@components/atoms/icon.js";
+import { Icons } from "@components/icons/icons.js";
+
+/**
+ * BrowsePage
+ *
+ * This will create the BrowsePage molecule.
+ *
+ * @returns {object}
+ */
+export const BrowsePage = () => {
+	return Div({
+			class: "flex flex-col items-center justify-center text-center p-6 space-y-4 bg-background border rounded-lg min-h-96 mt-4 mr-4",
+		},
+		[
+			Icon({ size: "xl" }, Icons.music),
+			H2({ class: "text-lg font-semibold" }, "The Browse Page"),
+			P({ class: "text-muted-foreground" }, "This is a placeholder for the browse page.")
+		]
+	);
+};
--- src/modules/main/components/pages/music/music-page/music-page.js
+++ src/modules/main/components/pages/music/music-page/music-page.js
@@ -1,5 +1,7 @@
 import { Div } from "@base-framework/atoms";
+import { Panel } from "@components/organisms/panel.js";
 import { BlankPage } from "@components/pages/blank-page.js";
+import { BrowsePage } from "./browse-page.js";
 import { MusicSections } from "./music-sections.js";
 import { SidebarMenu } from "./sidebar-menu.js";
 
@@ -14,9 +16,23 @@ export const MusicPage = () => (
     new BlankPage([
         Div({ class: 'grid grid-cols-1 lg:grid-cols-5 h-full' }, [
             SidebarMenu(),
-            Div({ class: 'col-span-4' }, [
-                MusicSections()
-            ])
+            Div({
+                class: 'col-span-4',
+                switch: [
+                    {
+                        uri: '/music',
+                        component: new Panel([
+                            MusicSections()
+                        ])
+                    },
+                    {
+                        uri: 'music/browse',
+                        component: new Panel([
+                            BrowsePage()
+                        ])
+                    }
+                ]
+            })
         ])
     ])
 );
