659bb4e715a9052bedd0eab2d8ba731067a064f1
tech_e <chris.durfee@dentalqore.com>
2024-11-25 16:33:35 -0700

    feat: Enhance InboxList component with tab navigation and dynamic title

--- src/modules/main/components/pages/inbox/inbox-list.js
+++ src/modules/main/components/pages/inbox/inbox-list.js
@@ -1,5 +1,5 @@
-import { Div, Span } from "@base-framework/atoms";
-import { Skeleton } from "@components/atoms/skeleton.js";
+import { Div, H4, Span } from "@base-framework/atoms";
+import TabGroup from "@components/organisms/tabs/tab-group.js";
 
 /**
  * This will create the InboxList organism.
@@ -10,14 +10,36 @@ import { Skeleton } from "@components/atoms/skeleton.js";
 export const InboxList = ({ messages }) => (
     Div({ class: "w-full space-y-2" }, [
         // List header
-        Div({ class: "p-4 border-b flex justify-between" }, [
-            Skeleton({ shape: "rectangle", width: "w-full", height: "h-10" }), // Replace with Input once loaded
+        Div({ class: "p-4" }, [
+            Div({
+                class: "flex justify-between",
+                useParent: ({ route }) =>
+                {
+                    const setTitle = (value = 'Inbox') => console.log(value);
+
+                    return [
+                        H4({ class: "font-semibold text-base text-foreground" }, [
+                            Span({
+                                text: ['[[page]]', route, setTitle],
+                            })
+                        ]),
+
+                        new TabGroup({
+                            options: [
+                                { label: 'All Mail', value: 'all' },
+                                { label: 'Unread', value: 'unread' }
+                            ],
+                            onSelect: (value) => console.log(value)
+                        })
+                    ];
+                }
+            })
         ]),
         // Messages
         Div({
             class: "space-y-2",
             map: [messages, (message) => InboxMessageItem({ message })],
-        }),
+        })
     ])
 );
 
