ed3eba8d1d6bfdac97352eb15ad503303b725045
chrisdurfee <chrisdurfee@users.noreply.github.com>
2024-11-26 02:16:06 +0000

    deploy: cf0de2e445c4609b4421ca6396af2f59ffad3708

---
+++ assets/sidebar-menu-BlFosvrZ.js
@@ -0,0 +1,1 @@
+import{m as o,U as s,C as e,ah as r}from"./index-DlC6Pni0.js";const b=({title:l,options:a})=>o({class:"pb-12 hidden lg:block p-6 border-r bg-sidebar max-w-[300px] h-full"},[l&&s({class:"pb-2 px-6 flex flex-col"},[e({class:"scroll-m-20 text-2xl font-bold tracking-tight"},l)]),new r({options:a})]);export{b as S};
