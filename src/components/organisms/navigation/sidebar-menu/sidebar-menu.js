67116fefc555e20978d4e2f3dacc1ed57a343602
tech_e <chris.durfee@dentalqore.com>
2024-11-26 22:28:09 -0700

    fix: Update SidebarMenu component to accept additional className prop for better customization

--- src/components/organisms/navigation/sidebar-menu/sidebar-menu.js
+++ src/components/organisms/navigation/sidebar-menu/sidebar-menu.js
@@ -7,8 +7,8 @@ import { InlineNavigation } from "../inline-navigation.js";
  * @param {object} props
  * @returns {object}
  */
-export const SidebarMenu = ({ title, options }) => (
-    Div({ class: 'pb-12 hidden lg:block p-6 border-r bg-sidebar max-w-[300px] h-full' }, [
+export const SidebarMenu = ({ title, options, class: className = '' }) => (
+    Div({ class: `pb-12 hidden lg:block p-6 border-r bg-sidebar max-w-[300px] h-full ${className}` }, [
         title && Header({ class: 'pb-2 px-6 flex flex-col' }, [
             H1({ class: 'scroll-m-20 text-2xl font-bold tracking-tight' }, title),
         ]),
