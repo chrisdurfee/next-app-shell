557afbe3dd82aeb6283a3a3e5d03a5bae4372e12
chrisdurfee <chrisdurfee@users.noreply.github.com>
2024-11-27 02:34:24 +0000

    deploy: e932cd3947711f9b81a6e1d3c96c96f5d624abe6

---
+++ assets/import-buttons-K-WFL7EM.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/button-group-h7Y7OSYr.js","assets/index-BNI3Am6O.js","assets/index-BqCoVQid.css"])))=>i.map(i=>d[i]);
+import{Q as t,m as s,F as e,S as o,V as r,p as i}from"./index-BNI3Am6O.js";const c=t((l,a)=>s([e({class:"scroll-m-20 text-lg font-bold tracking-tight"},"Label"),o({class:"text-muted-foreground"},"This section will test the import system."),r({src:i(()=>import("./button-group-h7Y7OSYr.js"),__vite__mapDeps([0,1,2]))})]));export{c as default};
