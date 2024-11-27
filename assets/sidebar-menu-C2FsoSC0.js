728caf026b7447afeba9b4dfa2055dca4682c9af
chrisdurfee <chrisdurfee@users.noreply.github.com>
2024-11-27 05:23:20 +0000

    deploy: 28c3a73f686633a0dccc330abbbfeb54f0996cb6

--- assets/sidebar-menu-C2FsoSC0.js
+++
@@ -1,1 +0,0 @@
-import{m as o,U as s,C as e,ai as r}from"./index-Bt_Jp67z.js";const n=({title:l,options:a})=>o({class:"pb-12 hidden lg:block p-6 border-r bg-sidebar max-w-[300px] h-full"},[l&&s({class:"pb-2 px-6 flex flex-col"},[e({class:"scroll-m-20 text-2xl font-bold tracking-tight"},l)]),new r({options:a})]);export{n as S};
