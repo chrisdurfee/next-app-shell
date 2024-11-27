ccd1ba20670100f420eec0343db2566a4e3d3f55
chrisdurfee <chrisdurfee@users.noreply.github.com>
2024-11-27 05:35:12 +0000

    deploy: dfca0ab0d4b045f860db05ff592fd1ec82bdc9ba

--- assets/sidebar-menu-xH1I8T_5.js
+++
@@ -1,1 +0,0 @@
-import{m as o,U as e,C as r,ai as c}from"./index-CKPX0MHB.js";const n=({title:l,options:a,class:s=""})=>o({class:`pb-12 hidden lg:block p-6 border-r bg-sidebar max-w-[300px] h-full ${s}`},[l&&e({class:"pb-2 px-6 flex flex-col"},[r({class:"scroll-m-20 text-2xl font-bold tracking-tight"},l)]),new c({options:a})]);export{n as S};
