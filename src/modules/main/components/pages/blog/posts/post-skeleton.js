b8b75e937d48351a9a9ca31974ee54e2e7516612
tech_e <chris.durfee@dentalqore.com>
2024-11-28 14:59:19 -0700

    fix: Remove unnecessary class from PostSkeleton for improved layout

--- src/modules/main/components/pages/blog/posts/post-skeleton.js
+++ src/modules/main/components/pages/blog/posts/post-skeleton.js
@@ -9,7 +9,7 @@ import { Skeleton } from "@components/atoms/skeleton.js";
  * @returns {object}
  */
 export const PostSkeleton = () => (
-    Div({ class: "flex flex-col items-center gap-6" }, [
+    Div({ class: "flex flex-col gap-6" }, [
         Skeleton({ width: "w-full", height: "h-96", class: "rounded-lg" }), // Image skeleton
         Skeleton({ width: "w-3/4", height: "h-8", class: "rounded-md" }), // Title skeleton
         Skeleton({ width: "w-1/2", height: "h-5", class: "rounded-md" }), // Metadata skeleton
