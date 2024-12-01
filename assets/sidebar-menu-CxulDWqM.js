6622a03d60b3f5e02bcc17074a8458f3fd230718
chrisdurfee <chrisdurfee@users.noreply.github.com>
2024-12-01 03:26:59 +0000

    deploy: ade6b73b66d137575e0968c2b2627b0b9c7c007a

--- assets/sidebar-menu-CxulDWqM.js
+++
@@ -1,1 +0,0 @@
-import{I as e,e as o,j as r,ai as t}from"./index-D_Zy5c60.js";const i=({title:a,options:s,class:l=""})=>e({class:`pb-12 p-6 border-r bg-sidebar max-w-[300px] h-full ${l}`},[a&&o({class:"pb-2 px-6 flex flex-col"},[r({class:"scroll-m-20 text-2xl font-bold tracking-tight"},a)]),new t({options:s})]);export{i as S};
