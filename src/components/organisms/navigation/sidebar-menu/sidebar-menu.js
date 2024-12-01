19a53871c16a39721eb62020f1ef35e4bc0736bd
tech_e <chris.durfee@dentalqore.com>
2024-12-01 16:53:54 -0700

    feat: Enhance settings page with back button and mobile navigation support

--- src/components/organisms/navigation/sidebar-menu/sidebar-menu.js
+++ src/components/organisms/navigation/sidebar-menu/sidebar-menu.js
@@ -8,7 +8,7 @@ import { InlineNavigation } from "../inline-navigation.js";
  * @returns {object}
  */
 export const SidebarMenu = ({ title, options, class: className = '' }) => (
-    Div({ class: `pb-12 p-4 lg:p-6 lg:border-r bg-sidebar lg:max-w-[300px] h-full ${className}` }, [
+    Div({ class: `pb-12 p-4 lg:p-6 lg:border-r lg:max-w-[300px] h-full ${className}` }, [
         title && Header({ class: 'pb-2 px-6 flex flex-col' }, [
             H1({ class: 'scroll-m-20 text-2xl font-bold tracking-tight' }, title),
         ]),
--- src/modules/main/components/pages/settings/settings-page.js
+++ src/modules/main/components/pages/settings/settings-page.js
@@ -1,6 +1,9 @@
 import { Div } from "@base-framework/atoms";
 import { Icons } from "@components/icons/icons.js";
+import { DockableOverlay } from "@components/organisms/overlays/dockable-overlay.js";
+import { BackButton } from "@components/organisms/overlays/overlay.js";
 import { Panel } from "@components/organisms/panel.js";
+import { BlankPage } from "@components/pages/blank-page.js";
 import { AccountSettings } from "./account-settings.js";
 import { AppearanceSettings } from "./appearance-settings.js";
 import { DisplaySettings } from "./display-settings.js";
@@ -17,6 +20,47 @@ import { SidebarMenu } from "./sidebar-menu.js";
 const SubRoute = (atom) => new Panel(atom);
 
 /**
+ * This will create the back button.
+ *
+ * @param {object} props
+ * @returns {object}
+ */
+export const PageToolbar = () => (
+    Div({ class: "flex items-center justify-between border-b pb-2" }, [
+        Div({ class: 'flex lg:hidden' }, [
+            BackButton({
+                margin: 'm-0 ml-0'
+            })
+        ])
+    ])
+);
+
+/**
+ * This will check if the window is in mobile view.
+ *
+ * @returns {boolean}
+ */
+const LG_WINDOW_SIZE = 1024;
+const isMObile = () => window.innerWidth < LG_WINDOW_SIZE;
+
+/**
+ * Props for the settings page.
+ *
+ * @type {object} Props
+ */
+const Props =
+{
+    afterSetup()
+    {
+        const page = this.route.page;
+        if (isMObile() && !page)
+        {
+            app.navigate('settings/profile');
+        }
+    }
+};
+
+/**
  * SettingsPage
  *
  * The main page layout for the Settings page with sidebar navigation and dynamic content sections.
@@ -24,31 +68,43 @@ const SubRoute = (atom) => new Panel(atom);
  * @returns {object}
  */
 export const SettingsPage = () => (
-    Div({ class: 'flex flex-auto flex-col lg:flex-row max-w-[100vw] h-full' }, [
-        // Sidebar
-        SidebarMenu({
-            title: 'Settings',
-            options: [
-                { label: 'Profile', href: 'settings', icon: Icons.user.default, exact: true },
-                { label: 'Account', href: 'settings/account', icon: Icons.locked },
-                { label: 'Appearance', href: 'settings/appearance', icon: Icons.sun },
-                { label: 'Notifications', href: 'settings/notifications', icon: Icons.bell.default },
-                { label: 'Display', href: 'settings/display', icon: Icons.window },
-            ]
-        }),
-
-        // Content Section
-        Div({ class: 'flex flex-auto flex-col p-4 lg:p-6' }, [
-            Div({
-                class: 'flex flex-auto flex-col contained',
-                switch: [
-                    { uri: '/settings/profile', component: SubRoute(ProfileSettings()) },
-                    { uri: '/settings/account', component: SubRoute(AccountSettings()) },
-                    { uri: '/settings/appearance', component: SubRoute(AppearanceSettings()) },
-                    { uri: '/settings/notifications', component: SubRoute(NotificationSettings()) },
-                    { uri: '/settings/display', component: SubRoute(DisplaySettings()) },
+    new BlankPage([
+        Div({ class: 'flex flex-auto flex-col lg:flex-row max-w-[100vw] h-full' }, [
+            // Sidebar
+            SidebarMenu({
+                title: 'Settings',
+                options: [
+                    { label: 'Profile', href: 'settings', icon: Icons.user.default, exact: true },
+                    { label: 'Account', href: 'settings/account', icon: Icons.locked },
+                    { label: 'Appearance', href: 'settings/appearance', icon: Icons.sun },
+                    { label: 'Notifications', href: 'settings/notifications', icon: Icons.bell.default },
+                    { label: 'Display', href: 'settings/display', icon: Icons.window },
                 ]
-            })
+            }),
+
+            // Content Section
+            Div({ class: 'flex flex-auto flex-col' }, [
+                Div({
+                    route: [
+                        {
+                            uri: 'settings/:page*',
+                            component: new DockableOverlay([
+                                PageToolbar(),
+                                Div({
+                                    class: 'flex flex-auto flex-col contained p-4 lg:p-6',
+                                    switch: [
+                                        { uri: '/settings/profile', component: SubRoute(ProfileSettings()) },
+                                        { uri: '/settings/account', component: SubRoute(AccountSettings()) },
+                                        { uri: '/settings/appearance', component: SubRoute(AppearanceSettings()) },
+                                        { uri: '/settings/notifications', component: SubRoute(NotificationSettings()) },
+                                        { uri: '/settings/display', component: SubRoute(DisplaySettings()) },
+                                    ]
+                                })
+                            ])
+                        }
+                    ]
+                })
+            ])
         ])
     ])
 );
