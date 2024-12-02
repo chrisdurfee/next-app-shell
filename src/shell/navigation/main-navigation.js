92111c095d331450eefe5f4e14d4763b2a9b9b4f
tech_e <chris.durfee@dentalqore.com>
2024-12-02 16:13:50 -0700

    feat: Update LowerNavigation to include settings link with icon and callback for navigation

--- src/shell/navigation/main-navigation.js
+++ src/shell/navigation/main-navigation.js
@@ -1,5 +1,6 @@
 import { Div } from "@base-framework/atoms";
 import { Atom, Jot } from "@base-framework/base";
+import { Icons } from "@components/icons/icons.js";
 import { InlineNavigation } from "@components/organisms/navigation/inline-navigation.js";
 import { NavigationAvatar } from "./avatars/navigation-avatar.js";
 import { MainHeader } from "./main-header.js";
@@ -37,7 +38,6 @@ const LowerNavigation = () => (
 		new InlineNavigation({
 			options: [
 				{
-					href: 'profile',
 					content: [
 
 						/**
@@ -46,7 +46,13 @@ const LowerNavigation = () => (
 						new NavigationAvatar({
 							data: app.data.user
 						})
-					]
+					],
+					callBack: () => app.navigate('settings/profile')
+				},
+				{
+					href: 'settings',
+					label: 'Settings',
+					icon: Icons.cog.eight
 				}
 			]
 		})
