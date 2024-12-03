2181adf69bed5fe47e7f06dbdf9589612e699f1e
tech_e <chris.durfee@dentalqore.com>
2024-12-02 19:46:37 -0700

    feat: Enhance data table with user information and avatar display

--- src/modules/main/components/pages/lists/data-table-page.js
+++ src/modules/main/components/pages/lists/data-table-page.js
@@ -1,32 +1,138 @@
-import { Div, Span, Td, Tr } from "@base-framework/atoms";
+import { Div, P, Span, Td, Tr } from "@base-framework/atoms";
+import { Atom } from "@base-framework/base";
 import { Button } from '@components/atoms/buttons/buttons.js';
 import { Checkbox } from '@components/atoms/form/checkbox.js';
+import { Avatar } from '@components/molecules/avatars/avatar.js';
 import { DataTable } from '@components/organisms/lists/data-table.js';
 import { BlankPage } from '@components/pages/blank-page.js';
 
 const headers = [
     { label: 'checkbox', key: '' },
+    { label: 'Name', key: 'name' },
     { label: 'Status', key: 'status' },
     { label: 'Email', key: 'email' },
     { label: 'Amount', key: 'amount', align: 'justify-end' }
 ];
 
 const rows = [
-    { id: 1, status: 'Success', email: 'ken99@yahoo.com', amount: 316.00, selected: false },
-    { id: 2, status: 'Success', email: 'abe45@gmail.com', amount: 242.00, selected: false },
-    { id: 3, status: 'Processing', email: 'monserrat44@gmail.com', amount: 837.00, selected: false },
-    { id: 4, status: 'Success', email: 'silas22@gmail.com', amount: 874.00, selected: false },
-    { id: 5, status: 'Failed', email: 'carmella@hotmail.com', amount: 721.00, selected: false },
-    // Add more rows as needed
+    {
+        id: 1,
+        status: 'Success',
+        email: 'leslie.alexander@example.com',
+        amount: 316.00,
+        selected: false,
+        name: 'Leslie Alexander',
+        image: 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        role: 'Co-Founder / CEO'
+    },
+    {
+        id: 2,
+        status: 'Success',
+        email: 'michael.foster@example.com',
+        amount: 242.00,
+        selected: false,
+        name: 'Michael Foster',
+        image: 'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        role: 'Co-Founder / CTO'
+    },
+    {
+        id: 3,
+        status: 'Processing',
+        email: 'dries.vincent@example.com',
+        amount: 837.00,
+        selected: false,
+        name: 'Dries Vincent',
+        image: 'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        role: 'Business Relations'
+    },
+    {
+        id: 4,
+        status: 'Success',
+        email: 'leslie.alexander@example.com',
+        amount: 874.00,
+        selected: false,
+        name: 'Leslie Alexander',
+        image: 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        role: 'Co-Founder / CEO'
+    },
+    {
+        id: 5,
+        status: 'Failed',
+        email: 'michael.foster@example.com',
+        amount: 721.00,
+        selected: false,
+        name: 'Michael Foster',
+        image: 'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        role: 'Co-Founder / CTO'
+    },
 ];
 
 const additionalRows = [
-    { id: 6, status: 'Success', email: '', amount: 0.00, selected: false },
-    { id: 7, status: 'Success', email: '', amount: 0.00, selected: false },
-    { id: 8, status: 'Processing', email: '', amount: 0.00, selected: false },
-    { id: 9, status: 'Success', email: '', amount: 0.00, selected: false },
-    { id: 10, status: 'Failed', email: '', amount: 0.00, selected: false }
+    {
+        id: 6,
+        status: 'Success',
+        email: 'dries.vincent@example.com',
+        amount: 0.00,
+        selected: false,
+        name: 'Dries Vincent',
+        image: 'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        role: 'Business Relations'
+    },
+    {
+        id: 7,
+        status: 'Success',
+        email: 'leslie.alexander@example.com',
+        amount: 0.00,
+        selected: false,
+        name: 'Leslie Alexander',
+        image: 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        role: 'Co-Founder / CEO'
+    },
+    {
+        id: 8,
+        status: 'Processing',
+        email: 'michael.foster@example.com',
+        amount: 0.00,
+        selected: false,
+        name: 'Michael Foster',
+        image: 'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        role: 'Co-Founder / CTO'
+    },
+    {
+        id: 9,
+        status: 'Success',
+        email: 'dries.vincent@example.com',
+        amount: 0.00,
+        selected: false,
+        name: 'Dries Vincent',
+        image: 'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        role: 'Business Relations'
+    },
+    {
+        id: 10,
+        status: 'Failed',
+        email: 'leslie.alexander@example.com',
+        amount: 0.00,
+        selected: false,
+        name: 'Leslie Alexander',
+        image: 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        role: 'Co-Founder / CEO'
+    },
 ];
+
+/**
+ * User Info Atom
+ *
+ * @param {object} props
+ * @returns {object}
+ */
+const UserInfo = Atom(({ name, email }) =>
+{
+    return Div({ class: 'min-w-0 flex-auto' }, [
+        P({ class: 'text-base font-semibold leading-6 m-0' }, name),
+        P({ class: 'truncate text-sm leading-5 text-muted-foreground m-0' }, email)
+    ]);
+});
 
 /**
  * This will create a row item.
@@ -44,6 +150,12 @@ const rowItem = (row, onSelect) =>
                 class: 'mr-2',
                 onChange: () => onSelect(row)
             })
+        ]),
+        Td({ class: 'p-4 ' }, [
+            Div({ class: 'flex min-w-0 gap-x-4' }, [
+                Avatar({ src: row.image, alt: row.name, fallbackText: row.name }),
+                UserInfo(row)
+            ])
         ]),
         Td({ class: 'p-4 ' }, [
             Span({ class: 'text-muted-foreground' }, row.status)
