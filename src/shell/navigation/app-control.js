d703362d0e2b323d9b63e0559968629f85bf33d9
tech_e <chris.durfee@dentalqore.com>
2024-12-08 16:00:19 -0700

    feat: Enhance mobile navigation with new styles and class handling for improved performance

--- src/shell/navigation/app-control.js
+++ src/shell/navigation/app-control.js
@@ -12,7 +12,7 @@ import { MobileNavigation } from "./mobile/mobile-navigation.js";
  */
 const AppContainer = Atom((props, children) => ({
     ...props,
-    class: 'app-nav-container bg-background/80 backdrop-blur-md fixed sm:top-0 bottom-0 left-0 w-full lg:w-[64px] lg:hover:w-[330px] sm:h-full z-10 lg:z-20 overflow-y-visible lg:overflow-y-auto lg:overflow-x-hidden shadow-md border-t sm:border-r sm:border-t-0 lg:border-r',
+    class: 'app-nav-container bg-background/80 backdrop-blur-md fixed sm:top-0 bottom-0 left-0 w-full lg:w-[64px] lg:hover:w-[330px] sm:h-full z-10 lg:z-20 lg:overflow-y-auto overflow-x-hidden shadow-md border-t sm:border-r sm:border-t-0 lg:border-r',
     children
 }));
 
