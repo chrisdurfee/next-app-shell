85810c68bbd508daaa8da6dec00ee40a6a81c524
tech_e <chris.durfee@dentalqore.com>
2024-12-10 02:14:21 -0700

    refactor: Remove unnecessary import and simplify PopOverContainer component

--- src/shell/navigation/mobile/popover-container.js
+++ src/shell/navigation/mobile/popover-container.js
@@ -1,4 +1,4 @@
-import { Div, OnState } from "@base-framework/atoms";
+import { OnState } from "@base-framework/atoms";
 import { PopOverNavigation } from "./popover-navigation.js";
 
 /**
@@ -7,15 +7,13 @@ import { PopOverNavigation } from "./popover-navigation.js";
  * @param {object} props
  * @returns {object}
  */
-export const PopOverContainer = ({ options, onSelect }) => (
-    Div([
-        OnState('open', (isOpen, ele, parent) => (!isOpen)
-            ? null
-            : new PopOverNavigation({
-                cache: 'popover',
-                parent: parent,
-                options
-            })
-        )
-    ])
+export const PopOverContainer = ({ options }) => (
+    OnState('open', (isOpen, ele, parent) => (!isOpen)
+        ? null
+        : new PopOverNavigation({
+            cache: 'popover',
+            parent: parent,
+            options
+        })
+    )
 );
