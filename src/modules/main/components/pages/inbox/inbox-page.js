7505b3b07ca0b821b9f1bd9c0b49476bab5835d3
tech_e <chris.durfee@dentalqore.com>
2024-12-10 01:44:47 -0700

    refactor: Improve structure and documentation in InboxPage component

--- src/modules/main/components/pages/inbox/inbox-page.js
+++ src/modules/main/components/pages/inbox/inbox-page.js
@@ -8,59 +8,60 @@ import { InboxSidebarMenu } from "./inbox-sidebar-menu.js";
 import { InboxList } from "./list/inbox-list.js";
 
 /**
- * @type {object} Props
- *
- * This will set up the page props.
- */
-const Props =
-{
-    /**
-     * This will set up the page data.
-     *
-     * @returns {object}
-     */
-    setData()
-    {
-        return new Data({
-            items: INBOX_MESSAGES
-        });
-    },
-
-    /**
-     * This will check to route to the inbox page if no page is set.
-     *
-     * @returns {void}
-     */
-    afterSetup()
-    {
-        if (!this.route.page)
-        {
-            app.navigate("inbox/inbox", null, true);
-        }
-    },
-
-    /**
-     * This will set up the page states.
-     *
-     * @returns {object}
-     */
-    setupStates()
-    {
-        return {
-            list: 'all'
-        };
-    }
-};
-
-/**
  * InboxPage
  *
  * This will create the Inbox page.
  *
  * @returns {object}
  */
-export const InboxPage = () => (
-    new BlankPage(Props, [
+export const InboxPage = () =>
+{
+    /**
+     * @type {object} Props
+     *
+     * This will set up the page props.
+     */
+    const Props =
+    {
+        /**
+         * This will set up the page data.
+         *
+         * @returns {object}
+         */
+        setData()
+        {
+            return new Data({
+                items: INBOX_MESSAGES
+            });
+        },
+
+        /**
+         * This will check to route to the inbox page if no page is set.
+         *
+         * @returns {void}
+         */
+        afterSetup()
+        {
+            if (!this.route.page)
+            {
+                app.navigate("inbox/inbox", null, true);
+            }
+        },
+
+        /**
+         * This will set up the page states.
+         *
+         * @returns {object}
+         */
+        setupStates()
+        {
+            return {
+                list: 'all'
+            };
+        }
+    };
+
+    return new BlankPage(Props, [
         Div({ class: "flex w-full flex-col lg:flex-row h-full" }, [
             InboxSidebarMenu(),
             Div({ class: "flex flex-[2] lg:max-w-[550px] lg:border-r" }, [
@@ -90,7 +91,7 @@ export const InboxPage = () => (
                 });
             })
         ])
-    ])
-);
+    ]);
+};
 
 export default InboxPage;
