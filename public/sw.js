fb58a9b08a7744b86d1ce8ca16a719cde0367e4a
tech_e <chris.durfee@dentalqore.com>
2024-12-09 18:13:35 -0700

    fix: Bump version number to 0.0.99 in service worker

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,7 @@
 importScripts('./worker/cache-controller.js', './worker/service.js', '.worker/push-controller.js');
 
 const appName = 'base-example',
-version = '0.0.98';
+version = '0.0.99';
 
 const files =
 [
