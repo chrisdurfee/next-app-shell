64d81d42a2313fcf8ee728075f6fc9043889a1bc
tech_e <chris.durfee@dentalqore.com>
2024-12-12 15:43:41 -0700

    feat: Add new components for documentation and example sections in HomePage

--- src/modules/main/components/pages/home/home-page.js
+++ src/modules/main/components/pages/home/home-page.js
@@ -1,5 +1,6 @@
-import { A, Div, H1, Header, P } from '@base-framework/atoms';
+import { A, Code, Div, H1, H3, Header, P, Pre, Section } from '@base-framework/atoms';
 import { Atom } from '@base-framework/base';
+import { Icons } from '@components/icons/icons.js';
 import { BlankPage } from '@components/pages/blank-page.js';
 
 /**
@@ -35,12 +36,41 @@ const PageHeader = Atom(({ title, description}) => (
 ));
 
 /**
+ * This will create a header for the documentation.
+ *
+ * @param {object} props
+ * @returns {object}
+ */
+export const SectionHeader = Atom(({ title, description}) => (
+	Header({ class: 'flex flex-col' }, [
+		H3({ class: 'scroll-m-20 text-2xl font-bold tracking-tight' }, title),
+		description && P({ class: 'text-base text-muted-foreground py-2 max-w-[700px]' }, description),
+	])
+));
+
+/**
+ * This will create a text section.
+ *
+ * @param {object} props
+ * @returns {object}
+ */
+const TextSection = Atom(({ title, description }, children) => (
+	Section({ class: 'flex flex-col w-full px-6 py-8 border-b' }, [
+		SectionHeader({
+			title,
+			description
+		}),
+		children
+	])
+));
+
+/**
  * This will create a main section.
  *
  * @param {object} props
  * @returns {object}
  */
-const MainSection = Atom((props) => (
+const MainSection = Atom((props, children) => (
 	Div({ class: 'flex flex-auto flex-col' }, [
 		Div({
 			class: 'flex flex-auto flex-col w-full max-w-[1400px] m-auto sm:pt-8 lg:border-r lg:border-l'
@@ -49,9 +79,31 @@ const MainSection = Atom((props) => (
 				title: 'Build faster with Base and Tailwind CSS',
 				description: 'Beautifully designed components inpired by the genius at Shadcn and tailored to work with Base. Base is easy to use and open source.'
 			}),
-			MainButtonContainer()
+			MainButtonContainer(),
+			...children
 		])
 	])
+));
+
+/**
+ * This will create a preview card.
+ *
+ * @param {object} props
+ * @param {object} children
+ * @returns {object}
+ */
+const CodeCard = Atom((props, children) => (
+	Pre({ ...props, class: `flex p-4 max-h-[650px] max-w-[1024px] overflow-x-auto rounded-lg border bg-muted whitespace-break-spaces break-all cursor-pointer mt-4 ${props.class}` }, [
+        Code({ class: 'font-mono flex-auto text-sm text-wrap', click: () =>
+        {
+            navigator.clipboard.writeText(children[0].textContent);
+            app.notify({
+                title: "Code copied",
+                description: "The code has been copied to your clipboard.",
+                icon: Icons.clipboard.checked
+            });
+        }}, children)
+    ])
 ));
 
 /**
@@ -64,7 +116,95 @@ const MainSection = Atom((props) => (
 export const HomePage = () => (
 	new BlankPage([
 		MainSection([
+			TextSection({
+				title: 'Why Base?',
+				description: 'Base is a small framework that does things like global and local states, routing, events, dates, data tracking, data binding, data watching, and rending of components using Atoms. It is designed to be simple and fast to build. It\'s open source and in npm.'
+			}, [
+				CodeCard('npm install @base-framework/base'),
+				Div({ class: 'mt-10 flex items-center justify-start gap-x-4 pb-6' }, [
+					A({
+						href: 'https://github.com/chrisdurfee/base/wiki',
+						target: '_blank',
+						class: 'bttn ghost gap-2'
+					}, 'Base Docs')
+				])
+			]),
+			TextSection({
+				title: 'Example App',
+				description: 'This is an example Todo app built with base.'
+			}, [
+				CodeCard(`import { Button, Div, Form, H1, Input, Li, Ul } from "@base-framework/atoms";
+import { Builder, Data } from "@base-framework/base";
 
+/**
+ * This will create a to-do app.
+ *
+ * @returns {object}
+ */
+export function ToDoApp()
+{
+    /**
+     * This will set up the data store for the to-do app.
+     */
+    const data = new Data({ items: [] });
+
+    /**
+     * This will handle the form submission for adding a new to-do item.
+     *
+     * @param {object} event
+     */
+    const handleSubmit = (event) =>
+    {
+        event.preventDefault();
+        const form = event.target;
+        const input = form.querySelector('input');
+
+        // add the new to-do item to the array of items
+        data.push('items', input.value);
+        input.value = '';
+    };
+
+    /**
+     * This will handle removing a to-do item from the list.
+     *
+     * @param {number} index
+     * @returns {boolean}
+     */
+    const handleRemove = (index) => data.splice('items', index);
+
+    return Div([
+        H1('To-Do App'),
+        Form({ submit: handleSubmit }, [
+            Input({ placeholder: 'Add a new item' }),
+            Button({ type: 'submit' }, 'Add')
+        ]),
+        Ul({
+            for: [data, 'items', (item, index) => Li([
+                Span(item),
+                Button({ click: () => handleRemove(index) }, 'Remove')
+            ])]
+        })
+    ]);
+}
+
+/**
+ * This will render the to-do app to the body of the document.
+ */
+Builder.render(ToDoApp(), document.body);`)
+			]),
+
+			TextSection({
+				title: 'Example Platform',
+			}, [
+				P({ class: 'text-base text-muted-foreground py-2 max-w-[700px]' }, "This app is an example of a platform that uses Base to render the app on the client-side. This responsive PWA (Progressive Web App) is installable and works on all devices. The app is built with Base and Tailwind CSS and is inspired by Shadcn UI. "),
+				Div({ class: 'mt-10 flex items-center justify-start gap-x-4 pb-6' }, [
+					A({
+						href: 'https://ui.shadcn.com/',
+						target: '_blank',
+						class: 'bttn ghost gap-2'
+					}, 'Shadcn UI')
+				])
+			])
 		])
 	])
 );
