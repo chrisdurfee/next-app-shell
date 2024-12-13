dd195a1ad2af6d4e6bd27b36dfb861bfb4d039dc
chrisdurfee <chrisdurfee@users.noreply.github.com>
2024-12-13 02:58:11 +0000

    deploy: 3ac116fd913f7ad5540875055f27d01c4850e553

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 importScripts('./worker/cache-controller.js', './worker/service.js', '.worker/push-controller.js');
 
 const appName = 'base-example',
-version = '0.0.108';
+version = '0.0.109';
 
 const files =
 [
