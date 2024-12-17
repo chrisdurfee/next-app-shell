6f4cf82c50b6a72ce08718c7404730796a6874b4
tech_e <chris.durfee@dentalqore.com>
2024-12-17 02:23:31 -0700

    refactor: Update Row component to enhance layout flexibility with flexbox

--- src/components/pages/templates/row.js
+++ src/components/pages/templates/row.js
@@ -9,7 +9,7 @@ import { Atom } from "@base-framework/base";
  */
 export const Row = Atom((props, children) =>
 {
-    props.class = 'row ' + (props.class || '');
+    props.class = 'row flex flex-auto flex-col lg:flex-row ' + (props.class || '');
 
     return {
         ...props,
