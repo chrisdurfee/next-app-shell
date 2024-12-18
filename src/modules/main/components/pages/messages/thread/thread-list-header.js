6e7518f9517532bd9882367ce517a681f1e5886c
tech_e <chris.durfee@dentalqore.com>
2024-12-17 18:21:03 -0700

    feat: Add AddMessageButton to ThreadListHeader for improved message management

--- src/modules/main/components/pages/messages/thread/thread-list-header.js
+++ src/modules/main/components/pages/messages/thread/thread-list-header.js
@@ -1,5 +1,21 @@
 import { Div, H2, Header } from "@base-framework/atoms";
+import { Button } from "@components/atoms/buttons/buttons.js";
+import { Tooltip } from "@components/atoms/tooltip.js";
+import { Icons } from "@components/icons/icons.js";
 import TabGroup from "@components/organisms/tabs/tab-group.js";
+
+/**
+ * This will create the AddMessageButton organism.
+ *
+ * @returns {object}
+ */
+const AddMessageButton = () => (
+    Div({ class: 'flex mr-2' }, [
+        Tooltip({ content: 'Add Message', position: 'left' }, [
+            Button({ variant: 'icon', class: 'primary', icon: Icons.circlePlus })
+        ])
+    ])
+);
 
 /**
  * ThreadListHeader
@@ -13,14 +29,20 @@ export const ThreadListHeader = () => (
         Div({ class: "flex justify-between items-center" }, [
             H2({ class: "pl-2 lg:pl-0 text-3xl lg:text-xl font-bold" }, "Messages"),
 
-            // Filter tabs: All / Unread
-            new TabGroup({
-                options: [
-                    { label: "All", value: "all" },
-                    { label: "Unread", value: "unread" },
-                ],
-                onSelect: (value, { state }) => state.filter = value
-            })
+            Div({ class: 'flex' }, [
+
+                AddMessageButton(),
+
+                Div({ class: 'hidden lg:flex' }, [
+                    new TabGroup({
+                        options: [
+                            { label: "All", value: "all" },
+                            { label: "Unread", value: "unread" },
+                        ],
+                        onSelect: (value, { state }) => state.filter = value
+                    })
+                ])
+            ])
         ])
     ])
 );
