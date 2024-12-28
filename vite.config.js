98fe2ec17421b9081a0d25f4c55785b90c52a80f
tech_e <chris.durfee@dentalqore.com>
2024-12-27 19:45:53 -0700

    fix: Update @base-framework/ui version to 0.0.3 in package.json and package-lock.json

--- vite.config.js
+++ vite.config.js
@@ -13,12 +13,6 @@ export default defineConfig({
 			'@components': path.resolve(__dirname, 'src/components'),
 			'@modules': path.resolve(__dirname, 'src/modules'),
 			'@shell': path.resolve(__dirname, 'src/shell'),
-			'@base-framework/ui/icons': path.resolve(__dirname, 'node_modules/@base-framework/ui/dist/icons.es.js'),
-			'@base-framework/ui/molecules': path.resolve(__dirname, 'node_modules/@base-framework/ui/dist/molecules.es.js'),
-			'@base-framework/ui/atoms': path.resolve(__dirname, 'node_modules/@base-framework/ui/dist/atoms.es.js'),
-			'@base-framework/ui/organisms': path.resolve(__dirname, 'node_modules/@base-framework/ui/dist/organisms.es.js'),
-			'@base-framework/ui/templates': path.resolve(__dirname, 'node_modules/@base-framework/ui/dist/templates.es.js'),
-			'@base-framework/ui/pages': path.resolve(__dirname, 'node_modules/@base-framework/ui/dist/pages.es.js'),
 		}
 	}
 });
