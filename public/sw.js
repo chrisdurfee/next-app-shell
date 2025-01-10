fee375143513748639e667604800bf5cbc2fdc59
tech_e <chris.durfee@dentalqore.com>
2025-01-10 13:39:24 -0700

    refactor: Update service worker to use CacheController for file management and increment version to 0.0.134

--- public/sw.js
+++ public/sw.js
@@ -15,7 +15,7 @@ const APP_NAME = 'base-example',
  * @const
  * @type {string} version
  */
-version = '0.0.133';
+version = '0.0.134';
 
 /**
  * This will add these files to cache. Add any files you want to
