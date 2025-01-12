79c460f9e84d90c923ef26e859a3da343ff4e7be
chrisdurfee <chrisdurfee@users.noreply.github.com>
2025-01-12 03:16:41 +0000

    deploy: b46cc92d2504cfe5c02eb73ecf11d2be8cf186ae

--- assets/about-page-OB22xarn.js
+++
@@ -1,1 +0,0 @@
-import{ah as t,Y as e}from"./index-DlwzRetq.js";const o=()=>new t({class:"flex flex-auto flex-col relative"},[e({class:"flex flex-auto flex-col items-center justify-center"},[e({class:"text-3xl font-semibold"},"About")])]);export{o as AboutPage,o as default};
