b46cc92d2504cfe5c02eb73ecf11d2be8cf186ae
tech_e <chris.durfee@dentalqore.com>
2025-01-11 20:15:54 -0700

    refactor: Simplify AlbumPageContent component by using OnRoute for route handling

--- src/modules/music/components/pages/album-page/album-page.js
+++ src/modules/music/components/pages/album-page/album-page.js
@@ -1,4 +1,4 @@
-import { Div, H2, Header } from "@base-framework/atoms";
+import { Div, H2, Header, OnRoute } from "@base-framework/atoms";
 import { BackButton, Overlay } from "@base-framework/ui/organisms";
 import { getAlbumByTitle } from "../albums.js";
 import { AlbumCard } from "./album-card.js";
@@ -13,26 +13,15 @@ import { TrackList } from "./track-list.js";
  *
  * @returns {object}
  */
-const AlbumPageContent = () =>
-{
-    return Div({
-        class: 'p-6 pt-0 2xl:mx-auto w-full 2xl:max-w-[1600px]',
-
-        /**
-         * This will get the parent object that has access to the route
-         * and use it to get the album details.
-         */
-        useParent({ route })
-        {
-            return Div({ class: 'flex flex-auto flex-col lg:flex-row lg:space-x-8', onSet: [route, 'album', (title) =>
+const AlbumPageContent = () => (
+    Div({ class: 'p-6 pt-0 2xl:mx-auto w-full 2xl:max-w-[1600px]' }, [
+        Div({ class: 'flex flex-auto flex-col lg:flex-row lg:space-x-8'}, [
+            OnRoute('album', (title) =>
             {
                 const album = getAlbumByTitle(title);
-                if (!album)
-                {
-                    return EmptyState();
-                }
-
-                return [
+                return (!album)
+                ? EmptyState()
+                : [
                     Div({ class: 'w-full lg:w-1/3' }, [
                         Header({ class: 'flex flex-auto items-center mb-2' }, [
                             BackButton({
@@ -47,10 +36,10 @@ const AlbumPageContent = () =>
                         TrackList({ tracks: album.tracks })
                     ])
                 ];
-            }]})
-        }
-    });
-};
+            })
+        ])
+    ])
+);
 
 /**
  * AlbumPage
