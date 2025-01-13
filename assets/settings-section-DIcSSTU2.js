6fd90cb10ce7672e1a949ee5b6b185ca2617f6bc
chrisdurfee <chrisdurfee@users.noreply.github.com>
2025-01-13 21:20:24 +0000

    deploy: 6617bf45cccee89072a8eabe8a9bec256e19035f

---
+++ assets/settings-section-DIcSSTU2.js
@@ -0,0 +1,1 @@
+import{Z as s,Y as i,G as a,a8 as l,b as c}from"./index-CQt2DmOM.js";const n=t=>c({class:"scroll-m-20 text-3xl font-bold tracking-tight"},t.title),m=s((t,e)=>i({class:`${t.class||""}`},[t.title&&n({title:t.title}),t.description&&a({class:"text-base text-muted-foreground py-2 max-w-[700px]"},t.description),l({class:"space-y-6 py-10",submit:t.submit},e)]));export{m as S};
