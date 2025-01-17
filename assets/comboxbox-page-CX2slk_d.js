cd49438d1df730cec8694d91bb298452fc6e740a
chrisdurfee <chrisdurfee@users.noreply.github.com>
2025-01-17 10:03:20 +0000

    deploy: 46ce0aa1cce7dad992a45d6b33b6d8a54d0c6fc8

---
+++ assets/comboxbox-page-CX2slk_d.js
@@ -0,0 +1,12 @@
+import{Y as o,aS as t,o as l}from"./index-DTTsA-UX.js";import{D as a}from"./doc-section-DiAFggAh.js";import{D as s}from"./doc-page-jt23TrgE.js";const i=[{value:"next.js",label:"Next.js",icon:l.home},{value:"sveltekit",label:"SvelteKit"},{value:"nuxt.js",label:"Nuxt.js"},{value:"remix",label:"Remix"},{value:"astro",label:"Astro"}],u=()=>s({title:"Comboboxes",description:"Comboboxes are used to allow users to select an option from a dropdown list or enter a custom value."},[a({title:"Usage",description:"To use the Combobox component, import it from the components library and use it in your application.",preview:[o({class:"w-full max-w-[300px]"},[new t({items:i,onSelect:e=>console.log(e)})])],code:`@base-framework/ui/molecules;
+import {  Icons  } from "@base-framework/ui/icons";
+
+new Combobox({
+    items: [
+    { value: 'next.js', label: 'Next.js', icon: Icons.home },,
+    { value: 'sveltekit', label: 'SvelteKit' },
+    { value: 'nuxt.js', label: 'Nuxt.js' },
+    { value: 'remix', label: 'Remix' },
+    { value: 'astro', label: 'Astro' },
+],
+})`})]);export{u as ComboboxPage,u as default};
