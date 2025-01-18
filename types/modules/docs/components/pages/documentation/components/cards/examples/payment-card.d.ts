44fecd1b5305cd11c74df692c5be7970ba322b0f
chrisdurfee <chrisdurfee@users.noreply.github.com>
2025-01-18 07:26:21 +0000

    deploy: 9c34b75f09af814fdb1bc97b1be4b3af5526637a

--- types/modules/docs/components/pages/documentation/components/cards/examples/payment-card.d.ts
+++ types/modules/docs/components/pages/documentation/components/cards/examples/payment-card.d.ts
@@ -3,8 +3,9 @@
  *
  * This will create a button group.
  *
- * @class
+ * @type {typeof Component}
  */
-export const PaymentMethoGroup: typeof import("@base-framework/base").Component;
+export const PaymentMethoGroup: typeof Component;
 export function PaymentCard(): object;
 export default PaymentCard;
+import { Component } from "@base-framework/base";
