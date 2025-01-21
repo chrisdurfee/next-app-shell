4f985b43979ef3aaf8ec20f2fb641beaa13133fc
chrisdurfee <chrisdurfee@users.noreply.github.com>
2025-01-21 22:10:21 +0000

    deploy: 757ad45475efffe9ed577408367e12600c45c444

--- sw.js
+++ sw.js
@@ -15,7 +15,7 @@ const APP_NAME = 'base-example',
  * @const
  * @type {string} version
  */
-version = '0.0.141';
+version = '0.0.142';
 
 /**
  * This will add these files to cache. Add any files you want to
