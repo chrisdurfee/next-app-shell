a5c4478393b0a054194bcfdcf71ca0cfd03d845a
chrisdurfee <chrisdurfee@users.noreply.github.com>
2025-01-22 20:59:08 +0000

    deploy: 6a14b2f5df1d4e97c16fc2ca574bf624c7b25e30

--- types/modules/messages/components/pages/video/video-chat-page.d.ts
+++ types/modules/messages/components/pages/video/video-chat-page.d.ts
@@ -1,5 +1,6 @@
-export function ParticipantTile({ name, avatarUrl, isSpeaking, muted }: object): object;
+export function VideoContainer({ name, isMuted, isMainParticipant }: object): object;
 export function Header({ title, participantCount }: object): object;
-export function FooterControls({ actions }: object): object;
+export function Controls({ actions }: object): object;
+export function ParticipantsList({ participants }: object): object;
 export function VideoChatPage(): object;
 export default VideoChatPage;
