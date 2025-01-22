45c01fa1e94a5b635fa1955c64c692d87f8779d1
chrisdurfee <chrisdurfee@users.noreply.github.com>
2025-01-22 23:56:12 +0000

    deploy: f54e839ecaf2734c6f015315bdaf2e4e9640129b

---
+++ assets/settings-section-Bu5d0mXt.js
@@ -0,0 +1,1 @@
+import{e as s,Y as a,G as i,a8 as l,a as c}from"./index-_r0u6tRE.js";const n=t=>c({class:"scroll-m-20 text-3xl font-bold tracking-tight"},t.title),m=s((t,e)=>a({class:`${t.class||""}`},[t.title&&n({title:t.title}),t.description&&i({class:"text-base text-muted-foreground py-2 max-w-[700px]"},t.description),l({class:"space-y-6 py-10",submit:t.submit},e)]));export{m as S};
