d50e5d269631639f86d55500584a19ba42465efa
tech_e <chris.durfee@dentalqore.com>
2025-01-23 14:17:30 -0700

    chore: Bump version to 0.0.144 in service worker

--- public/sw.js
+++ public/sw.js
@@ -15,7 +15,7 @@ const APP_NAME = 'base-example',
  * @const
  * @type {string} version
  */
-version = '0.0.143';
+version = '0.0.144';
 
 /**
  * This will add these files to cache. Add any files you want to
