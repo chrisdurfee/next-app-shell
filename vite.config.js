f784f229097fb708ba21aa97961ad213a622dea4
tech_e <chris.durfee@dentalqore.com>
2025-01-23 13:23:23 -0700

    feat: Update Vite configuration to use dynamic base URL from configs

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import path from 'path';
 import { defineConfig } from 'vite';
+import { Configs } from './src/configs.js';
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [],
-	base: '/next-app-shell/',
+	base: Configs.router.baseUrl,
 	server: {
 		open: true
 	},
