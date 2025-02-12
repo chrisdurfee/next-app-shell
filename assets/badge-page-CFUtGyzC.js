f4499f16accc1909c5c75f693911795d4bd49a8b
chrisdurfee <chrisdurfee@users.noreply.github.com>
2025-02-12 23:26:58 +0000

    deploy: e563f7cc171431ed896d6bf3978fe4e44ec5442b

---
+++ assets/badge-page-CFUtGyzC.js
@@ -0,0 +1,7 @@
+import{Y as t,au as e}from"./index-RbYAKzBL.js";import{D as p}from"./doc-section-BJYsMUS2.js";import{D as r}from"./doc-page-BaQxlDjw.js";const s=()=>r({title:"Badges",description:"Badges can be used to display a status or a count."},[p({title:"Badge",description:"This is a badge.",preview:[t({class:"flex gap-2 flex-wrap"},[e({type:"gray"},"Text"),e({type:"red"},"Text"),e({type:"yellow"},"Text"),e({type:"green"},"Text"),e({type:"blue"},"Text"),e({type:"indigo"},"Text"),e({type:"purple"},"Text"),e({type:"pink"},"Text"),e({type:"primary"},"Text"),e({type:"secondary"},"Text"),e({type:"destructive"},"Text"),e({type:"warning"},"Text"),e({type:"outline"},"Text"),e({type:"ghost"},"Text"),e({type:"link"},"Text")])],code:`
+import {  Button  } from "@base-framework/ui/atoms";
+import { Atom } from '@base-framework/base';
+
+export const PrimaryBadge = Atom((props, children)) => (
+    Badge({ ...props, type: 'gray' }, children)
+);`})]);export{s as BadgePage,s as default};
