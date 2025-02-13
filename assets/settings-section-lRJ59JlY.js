dd3314e9f3590aab0c03e84f424e05796dbf9b85
chrisdurfee <chrisdurfee@users.noreply.github.com>
2025-02-13 00:40:31 +0000

    deploy: 9941f38cdba1ce8509375297a6cd7cc1029f4147

--- assets/settings-section-lRJ59JlY.js
+++
@@ -1,1 +0,0 @@
-import{e as s,Y as a,a as i,G as l,a8 as c}from"./index-RbYAKzBL.js";const n=t=>i({class:"scroll-m-20 text-3xl font-bold tracking-tight"},t.title),m=s((t,e)=>a({class:`${t.class||""}`},[t.title&&n({title:t.title}),t.description&&l({class:"text-base text-muted-foreground py-2 max-w-[700px]"},t.description),c({class:"space-y-6 py-10",submit:t.submit},e)]));export{m as S};
