a100971c83cb547724943cc7e2191aedc7bf9aef
chrisdurfee <chrisdurfee@users.noreply.github.com>
2025-02-13 01:00:20 +0000

    deploy: 7d9c886f31cb99687a49a6bea029c4e5b1214648

---
+++ assets/about-page-BwHRY7wB.js
@@ -0,0 +1,1 @@
+import{aj as t,Y as e}from"./index-B1V06mH8.js";const o=()=>new t({class:"flex flex-auto flex-col relative"},[e({class:"flex flex-auto flex-col items-center justify-center"},[e({class:"text-3xl font-semibold"},"About")])]);export{o as AboutPage,o as default};
