b4dec0e11378ed0f27af23b78333291b8d442bca
chrisdurfee <chrisdurfee@users.noreply.github.com>
2025-02-13 00:50:04 +0000

    deploy: ae0097607dc53c3f9aaee5c638b92896789f4f12

---
+++ types/shell/login copy/sections/welcome-section.d.ts
@@ -0,0 +1,1 @@
+export function WelcomeSection(): object;
---
+++ types/shell/login copy/steps.d.ts
@@ -0,0 +1,6 @@
+/**
+ * This is a enum for the different steps in the sign-up process.
+ *
+ * @type {object} STEPS
+ */
+export const STEPS: object;
