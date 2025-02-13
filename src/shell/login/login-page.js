17f43cf751c2c39e6f222bee8b0d17b917e07c22
tech_e <chris.durfee@dentalqore.com>
2025-02-12 17:57:58 -0700

    fix: Add default export for LoginPage and SignUpPage components

--- src/shell/login/login-page.js
+++ src/shell/login/login-page.js
@@ -45,3 +45,5 @@ export const LoginPage = () => (
 		PageStepContainer()
 	])
 );
+
+export default LoginPage;
--- src/shell/sign-up/sign-up-page.js
+++ src/shell/sign-up/sign-up-page.js
@@ -45,3 +45,5 @@ export const SignUpPage = () => (
 		PageStepContainer()
 	])
 );
+
+export default SignUpPage;
