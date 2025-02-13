82052bbda9b0b89f9948a1750cf432044358e354
chrisdurfee <chrisdurfee@users.noreply.github.com>
2025-02-13 22:50:16 +0000

    deploy: 581f69bd69969d0ac6a457a03c3362cbe2e643fe

--- assets/settings-section-BleaCYtx.js
+++
@@ -1,1 +0,0 @@
-import{r as e,Y as a,a as i,G as c,ac as l}from"./index-_7WSbQ4O.js";const n=t=>i({class:"scroll-m-20 text-3xl font-bold tracking-tight"},t.title),m=e((t,s)=>a({class:`${t.class||""}`},[t.title&&n({title:t.title}),t.description&&c({class:"text-base text-muted-foreground py-2 max-w-[700px]"},t.description),l({class:"space-y-6 py-10",submit:t.submit},s)]));export{m as S};
