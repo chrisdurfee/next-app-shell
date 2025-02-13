4099bb07fc3966cb809e7dfd3880065d188ec8c5
tech_e <chris.durfee@dentalqore.com>
2025-02-13 14:29:42 -0700

    fix: Center header text in congratulations message component

--- src/shell/sign-up/atoms/congrats-message.js
+++ src/shell/sign-up/atoms/congrats-message.js
@@ -14,7 +14,7 @@ export const CongratsMessage = Atom(() =>
 	Div({ class: 'w-full max-w-sm bg-card text-card-foreground shadow rounded-xl sm:border sm:shadow-lg p-6' }, [
         Form({ class: 'flex flex-auto flex-col' }, [
             Div({ class: 'flex flex-auto flex-col space-y-4' }, [
-                Header({ class: 'py-4' }, [
+                Header({ class: 'py-4 text-center' }, [
                     H2({ class: 'text-xl font-bold' }, 'Congratulations!'),
                     P('Your account has been created successfully.'),
                 ]),
