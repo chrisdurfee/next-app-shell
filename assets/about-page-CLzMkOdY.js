e50fe70973500e5a61db8b421b0c5600e3968f21
chrisdurfee <chrisdurfee@users.noreply.github.com>
2025-02-14 20:32:47 +0000

    deploy: f932128f9a3152f1a6f6e093622debcf19c2117c

--- assets/about-page-CLzMkOdY.js
+++
@@ -1,1 +0,0 @@
-import{Y as e}from"./index-DDW7XYOj.js";import{n as t}from"./sidebar-menu-page-nCAVFk3R-CfzmPBT3.js";import"./mobile-nav-wrapper-BE_hr65B-BwlYgnmR.js";const s=()=>new t({class:"flex flex-auto flex-col relative"},[e({class:"flex flex-auto flex-col items-center justify-center"},[e({class:"text-3xl font-semibold"},"About")])]);export{s as AboutPage,s as default};
--- assets/badge-page-C1oDVwtw.js
+++
@@ -1,7 +0,0 @@
-import{Y as t,az as e}from"./index-DDW7XYOj.js";import{D as p}from"./doc-section-Cixoiu-a.js";import{D as r}from"./doc-page-Dos4Al3q.js";import"./sidebar-menu-page-nCAVFk3R-CfzmPBT3.js";import"./mobile-nav-wrapper-BE_hr65B-BwlYgnmR.js";const d=()=>r({title:"Badges",description:"Badges can be used to display a status or a count."},[p({title:"Badge",description:"This is a badge.",preview:[t({class:"flex gap-2 flex-wrap"},[e({type:"gray"},"Text"),e({type:"red"},"Text"),e({type:"yellow"},"Text"),e({type:"green"},"Text"),e({type:"blue"},"Text"),e({type:"indigo"},"Text"),e({type:"purple"},"Text"),e({type:"pink"},"Text"),e({type:"primary"},"Text"),e({type:"secondary"},"Text"),e({type:"destructive"},"Text"),e({type:"warning"},"Text"),e({type:"outline"},"Text"),e({type:"ghost"},"Text"),e({type:"link"},"Text")])],code:`
-import {  Button  } from "@base-framework/ui/atoms";
-import { Atom } from '@base-framework/base';
-
-export const PrimaryBadge = Atom((props, children)) => (
-    Badge({ ...props, type: 'gray' }, children)
-);`})]);export{d as BadgePage,d as default};
