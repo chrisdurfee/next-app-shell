486d34f2e1c90d998a91f999447b296a4d7685df
tech_e <chris.durfee@dentalqore.com>
2025-02-19 15:08:59 -0700

    refactor: Remove deprecated MULTI_FACTOR_AUTH step from login steps

--- src/shell/login/steps.js
+++ src/shell/login/steps.js
@@ -9,7 +9,6 @@ export const STEPS =
 	FORGOT_PASSWORD: 'forgot-password',
 	SIGN_UP: 'sign-up',
 	MULTI_FACTOR_METHOD: 'multi-factor-method',
-	MULTI_FACTOR_AUTH: 'multi-factor-auth',
 	CHANGE_PASSWORD: 'change-password',
 	ONE_TIME_CODE: 'one-time-code'
 };
