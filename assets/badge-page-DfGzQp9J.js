b7958d72678383e3ce6d6caf4597b34e747c964b
chrisdurfee <chrisdurfee@users.noreply.github.com>
2025-02-20 20:08:49 +0000

    deploy: dd52cf9ed6907034146c7b169e33e9cd9b2d8e11

---
+++ assets/badge-page-DfGzQp9J.js
@@ -0,0 +1,7 @@
+import{Y as t}from"./index-CVd7a0Z_.js";import{F as e}from"./tooltip-DSfp01wQ-ClOvK3DY.js";import{D as r}from"./doc-section-C4chk0Ds.js";import{D as o}from"./doc-page-FdvwJFlq.js";import"./signature-panel-CN_WhW5E-fqpffP2h.js";import"./sidebar-menu-page-nCAVFk3R-fok_J2-i.js";import"./mobile-nav-wrapper-BE_hr65B-BuZvu_91.js";const x=()=>o({title:"Badges",description:"Badges can be used to display a status or a count."},[r({title:"Badge",description:"This is a badge.",preview:[t({class:"flex gap-2 flex-wrap"},[e({type:"gray"},"Text"),e({type:"red"},"Text"),e({type:"yellow"},"Text"),e({type:"green"},"Text"),e({type:"blue"},"Text"),e({type:"indigo"},"Text"),e({type:"purple"},"Text"),e({type:"pink"},"Text"),e({type:"primary"},"Text"),e({type:"secondary"},"Text"),e({type:"destructive"},"Text"),e({type:"warning"},"Text"),e({type:"outline"},"Text"),e({type:"ghost"},"Text"),e({type:"link"},"Text")])],code:`
+import {  Button  } from "@base-framework/ui/atoms";
+import { Atom } from '@base-framework/base';
+
+export const PrimaryBadge = Atom((props, children)) => (
+    Badge({ ...props, type: 'gray' }, children)
+);`})]);export{x as BadgePage,x as default};
---
+++ assets/progress-bar-page-C9HjX5kr.js
@@ -0,0 +1,5 @@
+import{O as s,f as e}from"./tooltip-DSfp01wQ-ClOvK3DY.js";import"./index-CVd7a0Z_.js";import{D as r}from"./doc-section-C4chk0Ds.js";import{D as o}from"./doc-page-FdvwJFlq.js";import"./signature-panel-CN_WhW5E-fqpffP2h.js";import"./sidebar-menu-page-nCAVFk3R-fok_J2-i.js";import"./mobile-nav-wrapper-BE_hr65B-BuZvu_91.js";const n=()=>o({title:"Progress Bars",description:"Progress bars are a great way to show the progress of a task or process."},[r({title:"Usage",description:"Import the progress bar atom and use it in your components. The progress bar can be used to show the progress of a task or process.",preview:[new s({cache:"progress",progress:20})],code:`import {  ProgressBar  } from "@base-framework/ui/atoms";
+
+new ProgressBar({ cache: 'progress', progress: 20 })`}),r({title:"Circle Progress Bar",description:"The progress bar can also be displayed as a circle.",preview:[e({cache:"progress",progress:50,class:"stroke-gray-300"})],code:`import {  ProgressBar  } from "@base-framework/ui/atoms";
+
+new ProgressBar({ cache: 'progress', progress: 20 })`})]);export{n as ProgressBarPage,n as default};
