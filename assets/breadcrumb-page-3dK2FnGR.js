a243101dbc3acd3cdcd3d12435ce2d769620de1d
chrisdurfee <chrisdurfee@users.noreply.github.com>
2025-02-20 19:19:27 +0000

    deploy: 7035828e26080803048646951dbd4730db8991b5

---
+++ assets/breadcrumb-page-3dK2FnGR.js
@@ -0,0 +1,14 @@
+import{ai as a,ax as t}from"./index-CYev7DZz.js";import{D as e}from"./doc-section-ByVONLDt.js";import{D as i}from"./doc-page-C17UIuIA.js";import"./signature-panel-CN_WhW5E-BUOXOVgW.js";import"./tooltip-DSfp01wQ-Bp-A8kdE.js";import"./sidebar-menu-page-nCAVFk3R-CWHncTf_.js";import"./mobile-nav-wrapper-BE_hr65B-Dd3QyQx9.js";const r=[{href:"/",label:"Home"},{href:"/components",label:"Components"},{label:"Breadcrumb"}],u=()=>i({title:"Breadcrumbs",description:"Breadcrumbs are a navigation aid that helps users understand their location within a website or application."},[e({title:"Usage",description:"To use the Breadcrumb component, import it and provide an array of items. Each item can have a label and an optional href.",preview:[new a({items:r})],code:`import {  Breadcrumb  } from "@base-framework/ui/molecules";
+
+new Breadcrumb({ items: [
+    { href: '/', label: 'Home' },
+    { href: '/components', label: 'Components' },
+    { label: 'Breadcrumb' } // Last item, no href needed
+] })`}),e({title:"Dots Indicator",description:'The Dots Indicator component is used to display a row of circular dots, one of which is "active." The dots can be set to a specific size and count. The sizes are xs, sm, md, lg, xl, 2xl, and 3xl.',preview:[new t({count:6,size:"sm",gap:"gap-2",onClick:o=>console.log("Clicked dot",o)})],code:`import DotsIndicator from "@base-framework/ui/molecules";
+
+new DotsIndicator({
+    count: 6,
+    size: 'sm',
+    gap: 'gap-2',
+    onClick: (index) => console.log('Clicked dot', index)
+})`})]);export{u as BreadcrumbPage,u as default};
--- assets/progress-bar-page-BF-4mIfd.js
+++
@@ -1,5 +0,0 @@
-import{O as s,f as e}from"./tooltip-DSfp01wQ-BSv3X8ya.js";import"./index-CQMlnKgc.js";import{D as r}from"./doc-section-CBws0y3Y.js";import{D as o}from"./doc-page-C5RrmOm_.js";import"./signature-panel-CN_WhW5E-NmCLF8M2.js";import"./sidebar-menu-page-nCAVFk3R-C7yMsT-K.js";import"./mobile-nav-wrapper-BE_hr65B-CwyXzsPK.js";const n=()=>o({title:"Progress Bars",description:"Progress bars are a great way to show the progress of a task or process."},[r({title:"Usage",description:"Import the progress bar atom and use it in your components. The progress bar can be used to show the progress of a task or process.",preview:[new s({cache:"progress",progress:20})],code:`import {  ProgressBar  } from "@base-framework/ui/atoms";
-
-new ProgressBar({ cache: 'progress', progress: 20 })`}),r({title:"Circle Progress Bar",description:"The progress bar can also be displayed as a circle.",preview:[e({cache:"progress",progress:50,class:"stroke-gray-300"})],code:`import {  ProgressBar  } from "@base-framework/ui/atoms";
-
-new ProgressBar({ cache: 'progress', progress: 20 })`})]);export{n as ProgressBarPage,n as default};
