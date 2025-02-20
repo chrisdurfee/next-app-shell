dd52cf9ed6907034146c7b169e33e9cd9b2d8e11
tech_e <chris.durfee@dentalqore.com>
2025-02-20 13:08:12 -0700

    feat: Wrap ListenPage content in a Panel component for improved layout

--- src/modules/music/components/pages/music-page/listen/listen-page.js
+++ src/modules/music/components/pages/music-page/listen/listen-page.js
@@ -1,4 +1,5 @@
 import { Div, H1, Header } from "@base-framework/atoms";
+import { Panel } from "@base-framework/ui/organisms";
 import { MusicHeader } from "./music-header.js";
 import { MusicSections } from "./music/music-sections.js";
 import { PodcastSections } from "./podcasts/podcast-section.js";
@@ -11,19 +12,21 @@ import { PodcastSections } from "./podcasts/podcast-section.js";
  * @returns {object}
  */
 export const ListenPage = () => (
-	Div({ class: 'mx-auto max-w-[1400px]' }, [
-		Header({ class: 'sm:py-4 sm:pb-0 px-6 flex flex-col lg:hidden' }, [
-			H1({ class: 'scroll-m-20 text-3xl font-bold tracking-tight' }, 'Discover'),
-		]),
-		Div({ class: 'p-6 px-6 md:pr-4 py-6 lg:px-8'}, [
-			MusicHeader(),
-			Div({
-				addState() { return { mode: 'music' }},
-				onState: ['mode', (value) => (value === 'music')
-					? MusicSections()
-					: PodcastSections()
-				]
-			}),
+	new Panel([
+		Div({ class: 'mx-auto max-w-[1400px]' }, [
+			Header({ class: 'sm:py-4 sm:pb-0 px-6 flex flex-col lg:hidden' }, [
+				H1({ class: 'scroll-m-20 text-3xl font-bold tracking-tight' }, 'Discover'),
+			]),
+			Div({ class: 'p-6 px-6 md:pr-4 py-6 lg:px-8'}, [
+				MusicHeader(),
+				Div({
+					addState() { return { mode: 'music' }},
+					onState: ['mode', (value) => (value === 'music')
+						? MusicSections()
+						: PodcastSections()
+					]
+				}),
+			])
 		])
 	])
 );
