b62034279c6444242f0f8f61284c6abc2b5e7327
chrisdurfee <chrisdurfee@users.noreply.github.com>
2025-02-21 21:57:21 +0000

    deploy: 22e8de20ced4be14d98b4a73e5817f4b66f3f159

---
+++ assets/comboxbox-page-bSvy-YrJ.js
@@ -0,0 +1,12 @@
+import{Y as o,aw as t,o as l}from"./index-DZpPnb9k.js";import{D as a}from"./doc-section-BogJqfsU.js";import{D as s}from"./doc-page-Ca--KIUQ.js";import"./signature-panel-CN_WhW5E-BCBPU2wQ.js";import"./tooltip-DSfp01wQ-D7VXJKLG.js";import"./sidebar-menu-page-nCAVFk3R-DuA1DLkq.js";import"./mobile-nav-wrapper-BE_hr65B-Wukp6SHu.js";const i=[{value:"next.js",label:"Next.js",icon:l.home},{value:"sveltekit",label:"SvelteKit"},{value:"nuxt.js",label:"Nuxt.js"},{value:"remix",label:"Remix"},{value:"astro",label:"Astro"}],x=()=>s({title:"Comboboxes",description:"Comboboxes are used to allow users to select an option from a dropdown list or enter a custom value."},[a({title:"Usage",description:"To use the Combobox component, import it from the components library and use it in your application.",preview:[o({class:"w-full max-w-[300px]"},[new t({items:i,onSelect:e=>console.log(e)})])],code:`@base-framework/ui/molecules;
+import {  Icons  } from "@base-framework/ui/icons";
+
+new Combobox({
+    items: [
+    { value: 'next.js', label: 'Next.js', icon: Icons.home },,
+    { value: 'sveltekit', label: 'SvelteKit' },
+    { value: 'nuxt.js', label: 'Nuxt.js' },
+    { value: 'remix', label: 'Remix' },
+    { value: 'astro', label: 'Astro' },
+],
+})`})]);export{x as ComboboxPage,x as default};
--- assets/progress-bar-page-EALVfAHC.js
+++
@@ -1,5 +0,0 @@
-import{O as s,f as e}from"./tooltip-DSfp01wQ-BNg5pVIr.js";import"./index-Bwe8IfkH.js";import{D as r}from"./doc-section-Dl79jlbZ.js";import{D as o}from"./doc-page-BdgyJMuc.js";import"./signature-panel-CN_WhW5E-qyU7k5BQ.js";import"./sidebar-menu-page-nCAVFk3R-CU-HtvwU.js";import"./mobile-nav-wrapper-BE_hr65B-BP3wTZQk.js";const n=()=>o({title:"Progress Bars",description:"Progress bars are a great way to show the progress of a task or process."},[r({title:"Usage",description:"Import the progress bar atom and use it in your components. The progress bar can be used to show the progress of a task or process.",preview:[new s({cache:"progress",progress:20})],code:`import {  ProgressBar  } from "@base-framework/ui/atoms";
-
-new ProgressBar({ cache: 'progress', progress: 20 })`}),r({title:"Circle Progress Bar",description:"The progress bar can also be displayed as a circle.",preview:[e({cache:"progress",progress:50,class:"stroke-gray-300"})],code:`import {  ProgressBar  } from "@base-framework/ui/atoms";
-
-new ProgressBar({ cache: 'progress', progress: 20 })`})]);export{n as ProgressBarPage,n as default};
