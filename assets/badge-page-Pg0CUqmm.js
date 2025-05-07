fba36fbf184afd9e08f45dfc9e1e13f7b6af7e9e
chrisdurfee <chrisdurfee@users.noreply.github.com>
2025-05-07 01:28:14 +0000

    deploy: d3b58f7568198a931d9bc9187b9121d6adc65719

--- assets/badge-page-Pg0CUqmm.js
+++
@@ -1,7 +0,0 @@
-import{Y as t}from"./index-LBPNtWxk.js";import{T as e}from"./tooltip-Dxg9P7AJ-Dv49Rk19.js";import{D as r}from"./doc-section-9Pmb4HEX.js";import{D as p}from"./doc-page-DFnXWvsA.js";import"./signature-panel-BFels99c-B0-Phkm6.js";import"./sidebar-menu-page-BmD_Bi-v-BL7aAMa-.js";import"./mobile-nav-wrapper-BE_hr65B-BeLSWj8i.js";const x=()=>p({title:"Badges",description:"Badges can be used to display a status or a count."},[r({title:"Badge",description:"This is a badge.",preview:[t({class:"flex gap-2 flex-wrap"},[e({type:"gray"},"Text"),e({type:"red"},"Text"),e({type:"yellow"},"Text"),e({type:"green"},"Text"),e({type:"blue"},"Text"),e({type:"indigo"},"Text"),e({type:"purple"},"Text"),e({type:"pink"},"Text"),e({type:"primary"},"Text"),e({type:"secondary"},"Text"),e({type:"destructive"},"Text"),e({type:"warning"},"Text"),e({type:"outline"},"Text"),e({type:"ghost"},"Text"),e({type:"link"},"Text")])],code:`
-import { Badge } from "@base-framework/ui/atoms";
-import { Atom } from '@base-framework/base';
-
-export const PrimaryBadge = Atom((props, children)) => (
-    Badge({ ...props, type: 'gray' }, children)
-);`})]);export{x as BadgePage,x as default};
