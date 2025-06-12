c1476b82b8c54054b629dd63b64f81ac4d32e8c4
chrisdurfee <chrisdurfee@users.noreply.github.com>
2025-06-12 22:58:22 +0000

    deploy: b45c38eb620fe5a571c4c77f6243f18864f46367

---
+++ assets/comboxbox-page-jfa6h5en.js
@@ -0,0 +1,18 @@
+import{Y as o,ax as t,o as l}from"./index-Rc2MaJWI.js";import{D as a}from"./doc-section-D2QT9WP9.js";import{D as s}from"./doc-page-B3uJW97J.js";import"./signature-panel-Dl_Tkisq-BmR9zXfp.js";import"./tooltip-DcnXGw52-CoABw631.js";import"./sidebar-menu-page-D_2zNFuZ-B43kbhcW.js";import"./mobile-nav-wrapper-BWeAn5lg-D-XSmuK1.js";const i=[{value:"next.js",label:"Next.js",icon:l.home},{value:"sveltekit",label:"SvelteKit"},{value:"nuxt.js",label:"Nuxt.js"},{value:"remix",label:"Remix"},{value:"astro",label:"Astro"}],p=()=>s({title:"Comboboxes",description:"Comboboxes are used to allow users to select an option from a dropdown list or enter a custom value."},[a({title:"Usage",description:"To use the Combobox component, import it from the components library and use it in your application.",preview:[o({class:"w-full max-w-[300px]"},[new t({items:i,onSelect:e=>console.log(e)})])],code:`import { Combobox } from "@base-framework/ui/molecules";
+import { Icons } from "@base-framework/ui/icons";
+
+new Combobox({
+    width: 'w-full', // this is the default value
+    maxWidth: 'max-w-[250px]', // this is the default value
+    class: '',
+    onSelect: (item) => console.log(item),
+    selectedFirst: false,
+    required: false,
+    items: [
+        { value: 'next.js', label: 'Next.js', icon: Icons.home },
+        { value: 'sveltekit', label: 'SvelteKit' },
+        { value: 'nuxt.js', label: 'Nuxt.js' },
+        { value: 'remix', label: 'Remix' },
+        { value: 'astro', label: 'Astro' },
+    ],
+})`})]);export{p as ComboboxPage,p as default};
