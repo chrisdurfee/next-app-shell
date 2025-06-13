b79d0fce4dc1223a57e346df08ebf5c48f732f2e
tech_e <chris.durfee@dentalqore.com>
2025-06-12 22:49:43 -0600

    fix(email-empty-state): update icon and button variant for improved clarity

--- src/modules/inbox/components/pages/inbox/email/email-empty-state.js
+++ src/modules/inbox/components/pages/inbox/email/email-empty-state.js
@@ -10,13 +10,13 @@ import { Icons } from "@base-framework/ui/icons";
 export const EmailEmptyState = () => (
 	Div({ class: 'm-4 mt-8 p-4 rounded-md items-center justify-center text-center' }, [
 		Div({ class: 'flex-none max-w-12 mx-auto mt-8 mb-4' }, [
-			Icon({ size: "xl" }, Icons.document.text)
+			Icon({ size: "xl" }, Icons.envelope.open)
 		]),
 		H2({ class: 'text-xl font-semibold text-muted-foreground text-center' }, 'Nothing to see here.'),
 		Div({ class: 'flex flex-auto flex-col items-center justify-center' }, [
 			P('This view is empty. Click on a message is the list to view it or get started by creating a new message.')
 		]),
-		Button({ variant: 'outline', class: 'my-8' }, 'Create a Message')
+		Button({ variant: 'withIcon', class: 'my-8 outline', icon: Icons.circlePlus }, 'Create a Message')
 	])
 );
 
