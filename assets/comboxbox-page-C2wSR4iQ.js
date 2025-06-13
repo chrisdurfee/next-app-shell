292dbfa5b5a12d3e3714875e099de4a63d618510
chrisdurfee <chrisdurfee@users.noreply.github.com>
2025-06-13 05:35:08 +0000

    deploy: 428f8cf3e29007a520f35dee18cb2cff3f2f30ce

---
+++ assets/comboxbox-page-C2wSR4iQ.js
@@ -0,0 +1,18 @@
+import{Y as o,ax as t,o as l}from"./index-CrfBFc4r.js";import{D as a}from"./doc-section-CJ7S6Y76.js";import{D as s}from"./doc-page-B48Tc5uh.js";import"./signature-panel-Dl_Tkisq-C3ImX1ai.js";import"./tooltip-DcnXGw52-7gvqQSyu.js";import"./sidebar-menu-page-DyIwNFwe-BE9SdKXL.js";import"./mobile-nav-wrapper-CuATTfqK-8pG8N4jF.js";const i=[{value:"next.js",label:"Next.js",icon:l.home},{value:"sveltekit",label:"SvelteKit"},{value:"nuxt.js",label:"Nuxt.js"},{value:"remix",label:"Remix"},{value:"astro",label:"Astro"}],p=()=>s({title:"Comboboxes",description:"Comboboxes are used to allow users to select an option from a dropdown list or enter a custom value."},[a({title:"Usage",description:"To use the Combobox component, import it from the components library and use it in your application.",preview:[o({class:"w-full max-w-[300px]"},[new t({items:i,onSelect:e=>console.log(e)})])],code:`import { Combobox } from "@base-framework/ui/molecules";
+import { Icons } from "@base-framework/ui/icons";
+
+new Combobox({
+    width: 'w-full', // this is the default value
+    maxWidth: 'max-w-[250px]', // this is the default value
+    class: '',
+    onSelect: (item) => console.log(item),
+    selectedFirst: false,
+    required: false,
+    items: [
+        { value: 'next.js', label: 'Next.js', icon: Icons.home },
+        { value: 'sveltekit', label: 'SvelteKit' },
+        { value: 'nuxt.js', label: 'Nuxt.js' },
+        { value: 'remix', label: 'Remix' },
+        { value: 'astro', label: 'Astro' },
+    ],
+})`})]);export{p as ComboboxPage,p as default};
---
+++ assets/error-404-page-D3C2gtPX.js
@@ -0,0 +1,1 @@
+import{Y as e}from"./index-CrfBFc4r.js";import{c as t}from"./sidebar-menu-page-DyIwNFwe-BE9SdKXL.js";import"./mobile-nav-wrapper-CuATTfqK-8pG8N4jF.js";const s=()=>new t({class:"flex flex-auto flex-col relative"},[e({class:"flex flex-auto flex-col items-center justify-center"},[e({class:"text-3xl font-semibold"},"404 Not Found")])]);export{s as Error404Page,s as default};
