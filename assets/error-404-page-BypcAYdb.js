584150e214b894d7d2d34a8c6db74c805a358932
chrisdurfee <chrisdurfee@users.noreply.github.com>
2025-06-13 01:59:00 +0000

    deploy: 1c20f078f0241f68f8ed0ef943d7f7551ad3ec8f

---
+++ assets/error-404-page-BypcAYdb.js
@@ -0,0 +1,1 @@
+import{Y as e}from"./index-Br4mjmeG.js";import{c as t}from"./sidebar-menu-page-DyIwNFwe-BC5WVEoF.js";import"./mobile-nav-wrapper-CuATTfqK-BwN6puI3.js";const s=()=>new t({class:"flex flex-auto flex-col relative"},[e({class:"flex flex-auto flex-col items-center justify-center"},[e({class:"text-3xl font-semibold"},"404 Not Found")])]);export{s as Error404Page,s as default};
