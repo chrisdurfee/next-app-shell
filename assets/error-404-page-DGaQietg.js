bb1b64d83db8cd7b97375a59c1729997775c1e96
chrisdurfee <chrisdurfee@users.noreply.github.com>
2025-06-14 00:10:55 +0000

    deploy: 95636b36445b73eb92fe1052affdb670776e07cc

--- assets/error-404-page-DGaQietg.js
+++
@@ -1,1 +0,0 @@
-import{Y as e}from"./index-D5OCXbwU.js";import{c as t}from"./sidebar-menu-page-DyIwNFwe-CCLYdfA3.js";import"./mobile-nav-wrapper-CuATTfqK-IaYW16el.js";const s=()=>new t({class:"flex flex-auto flex-col relative"},[e({class:"flex flex-auto flex-col items-center justify-center"},[e({class:"text-3xl font-semibold"},"404 Not Found")])]);export{s as Error404Page,s as default};
