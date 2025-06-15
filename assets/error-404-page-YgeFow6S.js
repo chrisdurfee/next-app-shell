02cc20d8d7965004c7d2e880cc71c56aa3b156c2
chrisdurfee <chrisdurfee@users.noreply.github.com>
2025-06-15 00:15:58 +0000

    deploy: 36400beb6817a71e38ab1e0ff803ec874bd14798

--- assets/error-404-page-YgeFow6S.js
+++
@@ -1,1 +0,0 @@
-import{Y as e}from"./index-CXLgrEwp.js";import{c as t}from"./sidebar-menu-page-DyIwNFwe-BdpA43zQ.js";import"./mobile-nav-wrapper-CuATTfqK-B3ZAFyvX.js";const s=()=>new t({class:"flex flex-auto flex-col relative"},[e({class:"flex flex-auto flex-col items-center justify-center"},[e({class:"text-3xl font-semibold"},"404 Not Found")])]);export{s as Error404Page,s as default};
---
+++ types/modules/clients/components/pages/client/sections/conversation-section.d.ts
@@ -0,0 +1,10 @@
+/**
+ * ConversationSection
+ *
+ * Displays conversation history and composer.
+ *
+ * @param {object} props
+ * @param {object} props.client
+ * @returns {object}
+ */
+export const ConversationSection: Function;
