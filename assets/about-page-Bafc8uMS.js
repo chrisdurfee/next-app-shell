16aac56039dfd9a9f3bc78793516513f0bd72317
chrisdurfee <chrisdurfee@users.noreply.github.com>
2025-06-16 01:56:28 +0000

    deploy: 40cc1cb6a18793fb59e6147a47c9394b086dafbf

--- assets/about-page-Bafc8uMS.js
+++
@@ -1,1 +0,0 @@
-import{Y as e}from"./index-jCf4WMsB.js";import{c as t}from"./sidebar-menu-page-DyIwNFwe-vQ19D-5Z.js";import"./mobile-nav-wrapper-CuATTfqK-BH8fQkqG.js";const f=()=>new t({class:"flex flex-auto flex-col relative"},[e({class:"flex flex-auto flex-col items-center justify-center"},[e({class:"text-3xl font-semibold"},"About")])]);export{f as AboutPage,f as default};
