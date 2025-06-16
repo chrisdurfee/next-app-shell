709445b6d129da5566b6f11bf1996ceb48cc278b
tech_e <chris.durfee@dentalqore.com>
2025-06-16 02:13:14 -0600

    fix(page-header): remove unnecessary margin from Div component in invoice and payment headers

--- src/modules/clients/components/pages/client/billing/payments/page-header.js
+++ src/modules/clients/components/pages/client/billing/payments/page-header.js
@@ -7,7 +7,7 @@ import { Atom } from "@base-framework/base";
  * @returns {object}
  */
 export const PageHeader = Atom(() =>
-    Div({ class: "space-y-2 mb-8" }, [
+    Div({ class: "space-y-2" }, [
         H1({ class: "text-3xl font-bold tracking-tight" }, "Payment History")
     ])
 );
