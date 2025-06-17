b2e0c46ab498f0a0005ab70cbaee207cdb90af88
chrisdurfee <chrisdurfee@users.noreply.github.com>
2025-06-17 22:26:33 +0000

    deploy: 60787e8a36933987ef511c5370ad8401f5c79a27

---
+++ assets/error-404-page-CWxe-UVv.js
@@ -0,0 +1,1 @@
+import{Y as e}from"./index-CxhzxsYq.js";import{c as t}from"./sidebar-menu-page-B1YSAs3o-DFJZ2gFx.js";import"./mobile-nav-wrapper-m9eu2YJR-CPvjagWj.js";const s=()=>new t({class:"flex flex-auto flex-col relative"},[e({class:"flex flex-auto flex-col items-center justify-center"},[e({class:"text-3xl font-semibold"},"404 Not Found")])]);export{s as Error404Page,s as default};
