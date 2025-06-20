3f68199cdbd16b96a7259542e92f72da6c8a9af0
tech_e <chris.durfee@dentalqore.com>
2025-06-19 18:46:04 -0600

    fix: improve styling in Toolbar component for better layout and consistency

--- src/modules/directory/components/pages/directory/user/sidebar.js
+++ src/modules/directory/components/pages/directory/user/sidebar.js
@@ -12,14 +12,14 @@ import { UserAvatar } from "./user-avatar.js";
  * @returns {object}
  */
 const Toolbar = () => (
-	Div({ class: "flex w-full flex-col space-y-8 pb-2 mt-2 pl-2" }, [
+	Div({ class: "flex w-full flex-col space-y-8 pb-8 mt-2 pl-2" }, [
 		Div({ class: "flex items-center justify-between" }, [
-			Header({ class: 'flex space-x-4' }, [
+			Header({ class: 'flex space-x-4 items-center' }, [
 				BackButton({
 					margin: 'm-0 ml-0',
 					backUrl: '/directory'
 				}),
-				H1({ class: 'scroll-m-20 text-3xl lg:text-2xl font-bold tracking-tight' }, 'User Profile'),
+				H1({ class: 'scroll-m-20 text-2xl lg:text-lg font-bold tracking-tight' }, 'User Profile'),
 			])
 		]),
 		UserAvatar()
