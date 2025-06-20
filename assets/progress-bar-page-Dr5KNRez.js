598b87ed83feffdbb6f808168df92e0f6c5643af
chrisdurfee <chrisdurfee@users.noreply.github.com>
2025-06-20 00:46:41 +0000

    deploy: 3f68199cdbd16b96a7259542e92f72da6c8a9af0

---
+++ assets/progress-bar-page-Dr5KNRez.js
@@ -0,0 +1,5 @@
+import{f as s,O as e}from"./tooltip-CESEqMCU-aIB94oc5.js";import"./index-BT-4f13L.js";import{D as r}from"./doc-section-BSRe9IfQ.js";import{D as o}from"./doc-page-DCbkKOXa.js";import"./signature-panel-rfv7cz7U-DL0AmUBR.js";import"./sidebar-menu-page-BVryQj2Z-Bj9EeI0P.js";import"./mobile-nav-wrapper-Dj67Pb8l-ButTR5jG.js";const n=()=>o({title:"Progress Bars",description:"Progress bars are a great way to show the progress of a task or process."},[r({title:"Usage",description:"Import the progress bar atom and use it in your components. The progress bar can be used to show the progress of a task or process.",preview:[new s({cache:"progress",progress:20})],code:`import {  ProgressBar  } from "@base-framework/ui/atoms";
+
+new ProgressBar({ cache: 'progress', progress: 20 })`}),r({title:"Circle Progress Bar",description:"The progress bar can also be displayed as a circle.",preview:[e({cache:"progress",progress:50,class:"stroke-gray-300"})],code:`import {  ProgressBar  } from "@base-framework/ui/atoms";
+
+new ProgressBar({ cache: 'progress', progress: 20 })`})]);export{n as ProgressBarPage,n as default};
