5db2ab93f11947f712b86bc1f6ecface7fae1226
chrisdurfee <chrisdurfee@users.noreply.github.com>
2025-06-20 03:11:08 +0000

    deploy: d406578f4632c952fe5bd3cd1a395d405d353d3b

---
+++ assets/about-page-B9q1s5hq.js
@@ -0,0 +1,1 @@
+import{Y as e}from"./index-eDu3XvG2.js";import{c as t}from"./sidebar-menu-page-BVryQj2Z-BE3UVby4.js";import"./mobile-nav-wrapper-Dj67Pb8l-xipcQ-19.js";const s=()=>new t({class:"flex flex-auto flex-col relative"},[e({class:"flex flex-auto flex-col items-center justify-center"},[e({class:"text-3xl font-semibold"},"About")])]);export{s as AboutPage,s as default};
---
+++ assets/breadcrumb-page-M2Gp_U4Q.js
@@ -0,0 +1,14 @@
+import{aj as a,az as t}from"./index-eDu3XvG2.js";import{D as e}from"./doc-section-DCW8s7_X.js";import{D as i}from"./doc-page-e6Q1IVrV.js";import"./signature-panel-rfv7cz7U-BIZuKlMm.js";import"./tooltip-CESEqMCU-BozkQIjH.js";import"./sidebar-menu-page-BVryQj2Z-BE3UVby4.js";import"./mobile-nav-wrapper-Dj67Pb8l-xipcQ-19.js";const r=[{href:"/",label:"Home"},{href:"/components",label:"Components"},{label:"Breadcrumb"}],u=()=>i({title:"Breadcrumbs",description:"Breadcrumbs are a navigation aid that helps users understand their location within a website or application."},[e({title:"Usage",description:"To use the Breadcrumb component, import it and provide an array of items. Each item can have a label and an optional href.",preview:[new a({items:r})],code:`import {  Breadcrumb  } from "@base-framework/ui/molecules";
+
+new Breadcrumb({ items: [
+    { href: '/', label: 'Home' },
+    { href: '/components', label: 'Components' },
+    { label: 'Breadcrumb' } // Last item, no href needed
+] })`}),e({title:"Dots Indicator",description:'The Dots Indicator component is used to display a row of circular dots, one of which is "active." The dots can be set to a specific size and count. The sizes are xs, sm, md, lg, xl, 2xl, and 3xl.',preview:[new t({count:6,size:"sm",gap:"gap-2",onClick:o=>console.log("Clicked dot",o)})],code:`import DotsIndicator from "@base-framework/ui/molecules";
+
+new DotsIndicator({
+    count: 6,
+    size: 'sm',
+    gap: 'gap-2',
+    onClick: (index) => console.log('Clicked dot', index)
+})`})]);export{u as BreadcrumbPage,u as default};
