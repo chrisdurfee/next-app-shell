dbd929c945f7eb767cded2ceb1e3d21750ab9ce1
chrisdurfee <chrisdurfee@users.noreply.github.com>
2025-06-25 07:24:41 +0000

    deploy: d5708ddbdbec610f94f96d072e03f6e0d4ba2e33

--- assets/about-page-BVl-2TVS.js
+++
@@ -1,1 +0,0 @@
-import{Y as e}from"./index-CbGL_4kt.js";import{c as t}from"./sidebar-menu-page-BVryQj2Z-Do-zwJ__.js";import"./mobile-nav-wrapper-Dj67Pb8l-JT8-GhBM.js";const s=()=>new t({class:"flex flex-auto flex-col relative"},[e({class:"flex flex-auto flex-col items-center justify-center"},[e({class:"text-3xl font-semibold"},"About")])]);export{s as AboutPage,s as default};
