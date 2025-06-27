341d4c342af635489073843f2d423fa2663f1616
chrisdurfee <chrisdurfee@users.noreply.github.com>
2025-06-27 01:08:07 +0000

    deploy: 6af349fb88312e47fc84180f0591ded4702d6228

---
+++ assets/breadcrumb-page-BZun6PKq.js
@@ -0,0 +1,14 @@
+import{aj as a,aA as t}from"./index-OLu7ZXuG.js";import{D as e}from"./doc-section-CzcIrOdQ.js";import{D as i}from"./doc-page-BM-A088p.js";import"./signature-panel-C8UM0F7Y-CQ35E-QB.js";import"./tooltip-CESEqMCU-GdvZvUfN.js";import"./sidebar-menu-page-BVryQj2Z-BN57VDLf.js";import"./mobile-nav-wrapper-Dj67Pb8l-DXOLeg7H.js";const r=[{href:"/",label:"Home"},{href:"/components",label:"Components"},{label:"Breadcrumb"}],u=()=>i({title:"Breadcrumbs",description:"Breadcrumbs are a navigation aid that helps users understand their location within a website or application."},[e({title:"Usage",description:"To use the Breadcrumb component, import it and provide an array of items. Each item can have a label and an optional href.",preview:[new a({items:r})],code:`import {  Breadcrumb  } from "@base-framework/ui/molecules";
+
+new Breadcrumb({ items: [
+    { href: '/', label: 'Home' },
+    { href: '/components', label: 'Components' },
+    { label: 'Breadcrumb' } // Last item, no href needed
+] })`}),e({title:"Dots Indicator",description:'The Dots Indicator component is used to display a row of circular dots, one of which is "active." The dots can be set to a specific size and count. The sizes are xs, sm, md, lg, xl, 2xl, and 3xl.',preview:[new t({count:6,size:"sm",gap:"gap-2",onClick:o=>console.log("Clicked dot",o)})],code:`import DotsIndicator from "@base-framework/ui/molecules";
+
+new DotsIndicator({
+    count: 6,
+    size: 'sm',
+    gap: 'gap-2',
+    onClick: (index) => console.log('Clicked dot', index)
+})`})]);export{u as BreadcrumbPage,u as default};
