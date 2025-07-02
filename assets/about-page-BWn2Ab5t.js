b960259e8517613ff20b30b6d1eb3903370708ed
chrisdurfee <chrisdurfee@users.noreply.github.com>
2025-07-02 20:58:34 +0000

    deploy: 1d07beff7fc34af27ad9a1e68c44610e9b467b88

---
+++ assets/about-page-BWn2Ab5t.js
@@ -0,0 +1,1 @@
+import{J as e}from"./index-iT47V5ud.js";import{c as t}from"./sidebar-menu-page-BVryQj2Z-CMGrhGTP.js";import"./mobile-nav-wrapper-Dj67Pb8l-D8ixJoes.js";const s=()=>new t({class:"flex flex-auto flex-col relative"},[e({class:"flex flex-auto flex-col items-center justify-center"},[e({class:"text-3xl font-semibold"},"About")])]);export{s as AboutPage,s as default};
--- assets/progress-bar-page-zsnDhkvR.js
+++
@@ -1,5 +0,0 @@
-import{f as s,O as e}from"./tooltip-CESEqMCU-BFgby9v0.js";import"./index-WLhdxOcP.js";import{D as r}from"./doc-section-BpJHgPNa.js";import{D as o}from"./doc-page-CSuLSTxd.js";import"./signature-panel-C8UM0F7Y-CTyNLB9H.js";import"./sidebar-menu-page-BVryQj2Z-BXI7cWy_.js";import"./mobile-nav-wrapper-Dj67Pb8l-CaHKRCDm.js";const n=()=>o({title:"Progress Bars",description:"Progress bars are a great way to show the progress of a task or process."},[r({title:"Usage",description:"Import the progress bar atom and use it in your components. The progress bar can be used to show the progress of a task or process.",preview:[new s({cache:"progress",progress:20})],code:`import {  ProgressBar  } from "@base-framework/ui/atoms";
-
-new ProgressBar({ cache: 'progress', progress: 20 })`}),r({title:"Circle Progress Bar",description:"The progress bar can also be displayed as a circle.",preview:[e({cache:"progress",progress:50,class:"stroke-gray-300"})],code:`import {  ProgressBar  } from "@base-framework/ui/atoms";
-
-new ProgressBar({ cache: 'progress', progress: 20 })`})]);export{n as ProgressBarPage,n as default};
