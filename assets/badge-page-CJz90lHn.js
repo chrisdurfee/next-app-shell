d433e7823f1baa688b37d189e529f61731bd7dff
chrisdurfee <chrisdurfee@users.noreply.github.com>
2025-07-26 00:17:18 +0000

    deploy: 559131626b0d2452f9c5cb26c365b42f8427423f

--- assets/badge-page-CJz90lHn.js
+++
@@ -1,7 +0,0 @@
-import{J as t}from"./index-BnLBQANr.js";import{a as e}from"./tooltip-Ssw4NmQM-dV-_8IRf.js";import{D as r}from"./doc-section-C4e2mw69.js";import{D as p}from"./doc-page-CITjO0Ni.js";import"./signature-panel-Cj_uAlDW-BOMem9Tr.js";import"./sidebar-menu-page-BVryQj2Z-CCYkacrf.js";import"./mobile-nav-wrapper-Dj67Pb8l-BuWBWaZs.js";const x=()=>p({title:"Badges",description:"Badges can be used to display a status or a count."},[r({title:"Badge",description:"This is a badge.",preview:[t({class:"flex gap-2 flex-wrap"},[e({type:"gray"},"Text"),e({type:"red"},"Text"),e({type:"yellow"},"Text"),e({type:"green"},"Text"),e({type:"blue"},"Text"),e({type:"indigo"},"Text"),e({type:"purple"},"Text"),e({type:"pink"},"Text"),e({type:"primary"},"Text"),e({type:"secondary"},"Text"),e({type:"destructive"},"Text"),e({type:"warning"},"Text"),e({type:"outline"},"Text"),e({type:"ghost"},"Text"),e({type:"link"},"Text")])],code:`
-import { Badge } from "@base-framework/ui/atoms";
-import { Atom } from '@base-framework/base';
-
-export const PrimaryBadge = Atom((props, children)) => (
-    Badge({ ...props, type: 'gray' }, children)
-);`})]);export{x as BadgePage,x as default};
--- assets/notes-page-BqUo7hvz.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/notes-list-page-BffC187x.js","assets/index-BnLBQANr.js","assets/index-BMaXioJn.css","assets/tooltip-Ssw4NmQM-dV-_8IRf.js"])))=>i.map(i=>d[i]);
-import{J as o,c as r}from"./index-BnLBQANr.js";const s=(e,t)=>({uri:e,import:t}),c=()=>o({class:"flex flex-auto flex-col",switch:[s("/directory/user/:userId/notes",()=>r(()=>import("./notes-list-page-BffC187x.js"),__vite__mapDeps([0,1,2,3])))]});export{c as NotesPage,c as default};
