ea1ed0ff1b5065e214ad02e6fcc3571aa7474cdf
chrisdurfee <chrisdurfee@users.noreply.github.com>
2025-08-05 22:14:57 +0000

    deploy: f205c3f73f7301109d74f489f9134fd1b4daf748

---
+++ assets/notes-page-DZRN9Bpc.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/notes-list-page-DwpLUvzN.js","assets/index-YcJMzKrL.js","assets/index-tl1Zu2WV.css","assets/tooltip-Cok_NM7z-tw_85lnW.js"])))=>i.map(i=>d[i]);
+import{J as o,c as r}from"./index-YcJMzKrL.js";const s=(e,t)=>({uri:e,import:t}),c=()=>o({class:"flex flex-auto flex-col",switch:[s("/directory/user/:userId/notes",()=>r(()=>import("./notes-list-page-DwpLUvzN.js"),__vite__mapDeps([0,1,2,3])))]});export{c as NotesPage,c as default};
---
+++ assets/progress-bar-page-BiNi1tbz.js
@@ -0,0 +1,5 @@
+import{c as s,d as e}from"./tooltip-Cok_NM7z-tw_85lnW.js";import"./index-YcJMzKrL.js";import{D as r}from"./doc-section-BhrqZivH.js";import{D as o}from"./doc-page-BzzSJLhA.js";import"./signature-panel-BXbbFwMm-NNbh09FU.js";import"./sidebar-menu-page-BtUJdw-T-IA0pXlls.js";import"./mobile-nav-wrapper-C7RyGC5a-DR8JfFlJ.js";const n=()=>o({title:"Progress Bars",description:"Progress bars are a great way to show the progress of a task or process."},[r({title:"Usage",description:"Import the progress bar atom and use it in your components. The progress bar can be used to show the progress of a task or process.",preview:[new s({cache:"progress",progress:20})],code:`import {  ProgressBar  } from "@base-framework/ui/atoms";
+
+new ProgressBar({ cache: 'progress', progress: 20 })`}),r({title:"Circle Progress Bar",description:"The progress bar can also be displayed as a circle.",preview:[e({cache:"progress",progress:50,class:"stroke-gray-300"})],code:`import {  ProgressBar  } from "@base-framework/ui/atoms";
+
+new ProgressBar({ cache: 'progress', progress: 20 })`})]);export{n as ProgressBarPage,n as default};
